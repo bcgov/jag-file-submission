e6282be47ee500cd87ee4c66492e353d951ac7e2
alex <alex.joyeux@gov.bc.ca>
2020-06-08 14:25:03 -0700

    Adding generateUrl endpoint

---
+++ src/backend/jag-efiling-api/src/test/java/ca/bc/gov/open/jagefilingapi/document/DocumentApiImplTest.java
@@ -0,0 +1,54 @@
+package ca.bc.gov.open.jagefilingapi.document;
+
+import ca.bc.gov.open.api.model.GenerateUrlRequest;
+import ca.bc.gov.open.api.model.GenerateUrlResponse;
+import ca.bc.gov.open.api.model.Navigation;
+import ca.bc.gov.open.api.model.Redirect;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DisplayName("DocumentApiImpl Test Suite")
+public class DocumentApiImplTest {
+
+    private DocumentApiImpl sut;
+
+
+    @BeforeAll
+    public void setUp() {
+        sut = new DocumentApiImpl();
+    }
+
+
+    @Test
+    @DisplayName("CASE1: when payload is valid")
+    public void withValidPayloadShouldReturnOk() {
+
+        GenerateUrlRequest generateUrlRequest = new GenerateUrlRequest();
+        Navigation navigation = new Navigation();
+        Redirect successRedirect = new Redirect();
+        successRedirect.setUrl("CASE1");
+        navigation.setSuccess(successRedirect);
+        Redirect cancelRedirect = new Redirect();
+        cancelRedirect.setUrl("cancel");
+        navigation.setCancel(cancelRedirect);
+        Redirect errorRedirect = new Redirect();
+        navigation.setError(errorRedirect);
+        generateUrlRequest.setNavigation(navigation);
+
+        ResponseEntity<GenerateUrlResponse> actual = sut.generateUrl(generateUrlRequest);
+
+        assertEquals(HttpStatus.OK, actual.getStatusCode());
+        assertEquals("https://httpbin.org/", actual.getBody().getEFilingUrl());
+
+    }
+
+
+
+}
