ebb9dc013a74dfe5686b8763816f3e0da4325f75
alex <alex.joyeux@gov.bc.ca>
2020-06-09 11:27:00 -0700

    added returnurl as property

---
+++ src/backend/jag-efiling-api/README.md
@@ -0,0 +1,9 @@
+# Jag-efiling-api
+
+### Configuration
+
+You should use environment variables to configure the jag efiling api
+
+| Environment Variable  | Description   | Notes   |
+|---|---|---|
+| NAVIGATION_BASE_URL | The base path of the secure file upload  |  not set by default |
--- src/backend/jag-efiling-api/src/main/java/ca/bc/gov/open/jagefilingapi/document/DocumentApiImpl.java
+++ src/backend/jag-efiling-api/src/main/java/ca/bc/gov/open/jagefilingapi/document/DocumentApiImpl.java
@@ -3,20 +3,29 @@ package ca.bc.gov.open.jagefilingapi.document;
 import ca.bc.gov.open.api.DocumentApi;
 import ca.bc.gov.open.api.model.GenerateUrlRequest;
 import ca.bc.gov.open.api.model.GenerateUrlResponse;
+import ca.bc.gov.open.jagefilingapi.config.NavigationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
 @RestController
+@EnableConfigurationProperties(NavigationProperties.class)
 public class DocumentApiImpl implements DocumentApi {
+
+    private final NavigationProperties navigationProperties;
+
+    public DocumentApiImpl(NavigationProperties navigationProperties) {
+        this.navigationProperties = navigationProperties;
+    }
 
     @Override
     public ResponseEntity<GenerateUrlResponse> generateUrl(@Valid GenerateUrlRequest generateUrlRequest) {
 
         GenerateUrlResponse response = new GenerateUrlResponse();
 
-        response.setEFilingUrl("https://httpbin.org/");
+        response.setEFilingUrl(navigationProperties.getBaseUrl());
 
         return ResponseEntity.ok(response);
 
