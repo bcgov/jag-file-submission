c436283e8cd8e028bb568498b9f71d452fdd7883
Shreyas Devalapurkar <shreyas.devalapurkar@gov.bc.ca>
2020-06-10 13:40:18 -0700

    fix storyshots and add gov theme properly

--- src/efiling-frontend/src/index.js
+++ src/efiling-frontend/src/index.js
@@ -4,6 +4,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import "@bcgov/bootstrap-theme/dist/css/bootstrap-theme.min.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
