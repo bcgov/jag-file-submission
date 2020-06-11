00dde1632674cc7d363a215fa8ebbc73615b5608
alex <alex.joyeux@gov.bc.ca>
2020-06-11 06:33:18 -0700

    Added missing import

--- src/backend/jag-efiling-api/src/test/java/ca/bc/gov/open/jagefilingapi/document/DocumentApiImplTest.java
+++ src/backend/jag-efiling-api/src/test/java/ca/bc/gov/open/jagefilingapi/document/DocumentApiImplTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
