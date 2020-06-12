8aff44f5b4a5cb1279d32d5199e53cd7ab868d67
Shreyas Devalapurkar <shreyas.devalapurkar@gov.bc.ca>
2020-06-12 11:40:03 -0700

    try to fix duplication in header

--- src/frontend/efiling-frontend/src/components/base/header/Header.js
+++ src/frontend/efiling-frontend/src/components/base/header/Header.js
@@ -4,43 +4,41 @@ import PropTypes from "prop-types";
 import { useHistory } from "react-router-dom";
 import "./Header.css";
 
+export const HeadingTitle = (history, classNames) => {
+  return (
+    <div
+      className={classNames}
+      onClick={() => history.push("/")}
+      role="button"
+      onKeyDown={() => history.push("/")}
+      tabIndex={0}
+    />
+  );
+};
+
 export default function Header({ header: { name } }) {
   const history = useHistory();
 
   return (
     <header>
       <nav className="navbar navbar-expand-lg navbar-dark">
-        <div
-          className="container-fluid navbar-brand pointer"
-          onClick={() => history.push("/")}
-          role="button"
-          onKeyDown={() => history.push("/")}
-          tabIndex={0}
-        >
-          <img
-            className="img-fluid d-none d-md-block"
-            src={`${process.env.PUBLIC_URL}/images/bcid-logo-rev-en.svg`}
-            width="181"
-            height="44"
-            alt="B.C. Government Logo"
-          />
-          <img
-            className="img-fluid d-md-none"
-            src={`${process.env.PUBLIC_URL}/images/bcid-symbol-rev.svg`}
-            width="64"
-            height="44"
-            alt="B.C. Government Logo"
-          />
-          <div
-            className="pointer navbar-brand nav-item nav-link"
-            onClick={() => history.push("/")}
-            role="button"
-            onKeyDown={() => history.push("/")}
-            tabIndex={0}
-          >
-            {name}
-          </div>
-        </div>
+        {HeadingTitle(history, "container-fluid navbar-brand pointer")}
+        <img
+          className="img-fluid d-none d-md-block"
+          src={`${process.env.PUBLIC_URL}/images/bcid-logo-rev-en.svg`}
+          width="181"
+          height="44"
+          alt="B.C. Government Logo"
+        />
+        <img
+          className="img-fluid d-md-none"
+          src={`${process.env.PUBLIC_URL}/images/bcid-symbol-rev.svg`}
+          width="64"
+          height="44"
+          alt="B.C. Government Logo"
+        />
+        {HeadingTitle(history, "pointer navbar-brand nav-item nav-link")}
+        <div className="navbar-brand">{name}</div>
       </nav>
     </header>
   );
