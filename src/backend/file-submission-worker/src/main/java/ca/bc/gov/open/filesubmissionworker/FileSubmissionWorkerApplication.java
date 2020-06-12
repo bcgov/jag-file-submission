88e2eba5b41bb17e34e505f68e3bbfd4344c95ee
TayGov <Taylor.Clausen@gov.bc.ca>
2020-06-12 09:18:22 -0700

    Adding file submission worker
    Also one sonar issue

---
+++ src/backend/file-submission-worker/src/main/java/ca/bc/gov/open/filesubmissionworker/FileSubmissionWorkerApplication.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.open.filesubmissionworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSubmissionWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileSubmissionWorkerApplication.class, args);
+	}
+
+}
