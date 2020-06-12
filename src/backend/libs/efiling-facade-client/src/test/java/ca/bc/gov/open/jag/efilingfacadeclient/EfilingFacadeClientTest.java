7b3abe7c31c3208988158608c5204bcb4c90e73c
Kyle Flood <Kyle.Flood@nttdata.com>
2020-06-12 10:46:32 -0700

    EfilingClient
    Efiling service, client and WSDL parsing.

---
+++ src/backend/libs/efiling-facade-client/src/test/java/ca/bc/gov/open/jag/efilingfacadeclient/EfilingFacadeClientTest.java
@@ -0,0 +1,28 @@
+package ca.bc.gov.open.jag.efilingfacadeclient;
+
+
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.math.BigDecimal;
+
+
+
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DisplayName("Efiling Facade Client Test Suite")
+public class EfilingFacadeClientTest {
+
+
+    @DisplayName("CASE 1:  Calling SubmitFiling should return a BigDecimal of ZERO")
+    @Test
+    public void callSubmitFilingTest() {
+
+        EfilingFacadeClient client = new EfilingFacadeClient();
+        BigDecimal ret = client.SubmitFiling("File");
+
+        assertEquals(BigDecimal.ZERO, ret);
+    }
+
+}
