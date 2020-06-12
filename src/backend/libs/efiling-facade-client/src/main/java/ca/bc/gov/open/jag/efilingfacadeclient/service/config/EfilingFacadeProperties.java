ed117852b1e4bc388c5bda819e2456dbb9bb4981
Kyle Flood <Kyle.Flood@nttdata.com>
2020-06-12 12:10:40 -0700

    PR Comments
    PR Comments

---
+++ src/backend/libs/efiling-facade-client/src/main/java/ca/bc/gov/open/jag/efilingfacadeclient/service/config/EfilingFacadeProperties.java
@@ -0,0 +1,17 @@
+package ca.bc.gov.open.jag.efilingfacadeclient.service.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "jag.efiling.client")
+public class EfilingFacadeProperties {
+
+    private String filingFacadeSoapUri;
+
+    public String getFilingFacadeSoapUri() {
+        return filingFacadeSoapUri;
+    }
+
+    public void setFilingFacadeSoapUri(String filingFacadeSoapUri) {
+        this.filingFacadeSoapUri = filingFacadeSoapUri;
+    }
+}
