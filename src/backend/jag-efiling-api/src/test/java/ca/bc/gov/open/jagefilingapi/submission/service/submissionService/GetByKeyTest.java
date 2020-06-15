2aefe7f9eabed0be283ec746f77394bcc2322121
alex <alex.joyeux@gov.bc.ca>
2020-06-15 09:27:26 -0700

    fixed unit tests

--- src/backend/jag-efiling-api/src/test/java/ca/bc/gov/open/jagefilingapi/submission/service/submissionService/GetByKeyTest.java
+++ src/backend/jag-efiling-api/src/test/java/ca/bc/gov/open/jagefilingapi/submission/service/submissionService/GetByKeyTest.java
@@ -15,13 +15,9 @@ public class GetByKeyTest {
 
     private static final UUID ID = UUID.randomUUID();
     private static final String TYPE = "type";
-    public static final UUID CASE_2 = UUID.randomUUID();
 
     @InjectMocks
     private SubmissionServiceImpl sut;
-
-//    @Mock
-//    public StorageService<Submission> storageServiceMock;
 
     @BeforeAll
     public void setUp() {
@@ -30,31 +26,15 @@ public class GetByKeyTest {
 
         DocumentProperties documentMetadata = new DocumentProperties();
         documentMetadata.setType(TYPE);
-     //   Mockito.when(storageServiceMock.getByKey(ArgumentMatchers.eq(ID))).thenReturn(Submission.builder().documentProperties(documentMetadata).create());
-
     }
 
     @Test
-    @DisplayName("CASE 1: with valid id should return submission")
+    @DisplayName("CASE 1: without cache always return empty")
     public void withExistingIdShouldReturnSubmission() {
 
         Optional<Submission> actual = sut.getByKey(ID);
-
-        Assertions.assertEquals(TYPE, actual.get().getDocumentProperties().getType());
-
-    }
-
-    @Test
-    @DisplayName("CASE 2: with not found object should return null")
-    public void withMissingObjectShouldReturnNull() {
-
-        Optional<Submission> actual = sut.getByKey(CASE_2);
-
-        Assertions.assertFalse(actual.isPresent());
+        Assertions.assertEquals(Optional.empty(), actual);
 
     }
-
-
-
 
 }
