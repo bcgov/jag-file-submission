38a49f204fcc491041fb99bb1051123aa8a9ed3d
alex <alexandre.joyeux@nttdata.com>
2020-06-15 06:59:46 -0700

    Demo: added unit tests

--- src/backend/efiling-backend-demo/src/main/java/ca/bc/gov/open/jag/efilingbackenddemo/DocumentService.java
+++ src/backend/efiling-backend-demo/src/main/java/ca/bc/gov/open/jag/efilingbackenddemo/DocumentService.java
@@ -1,7 +1,6 @@
 package ca.bc.gov.open.jag.efilingbackenddemo;
 
 import ca.bc.gov.open.jag.efilingbackenddemo.api.DocumentApiDelegate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.ResponseEntity;
@@ -10,8 +9,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class DocumentService implements DocumentApiDelegate {
 
-    @Autowired
-    ResourceLoader resourceLoader;
+    private final ResourceLoader resourceLoader;
+
+    public DocumentService(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
 
     @Override
     public ResponseEntity<Resource> getDocumentById(String id) {
