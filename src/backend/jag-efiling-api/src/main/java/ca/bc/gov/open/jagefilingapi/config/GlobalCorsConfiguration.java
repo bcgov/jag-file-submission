c9407edc7c9690fe9bd71a9b80aa28a9135a8f1e
Shreyas Devalapurkar <shreyas.devalapurkar@gov.bc.ca>
2020-06-17 16:20:23 -0700

    make backend call to generate url

---
+++ src/backend/jag-efiling-api/src/main/java/ca/bc/gov/open/jagefilingapi/config/GlobalCorsConfiguration.java
@@ -0,0 +1,21 @@
+package ca.bc.gov.open.jagefilingapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class GlobalCorsConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("GET", "POST", "OPTIONS")
+                        .allowedOrigins("http://localhost:3000", "http://localhost:3001");
+            }
+        };
+    }
+}
--- src/frontend/efiling-demo/src/components/page/home/Home.js
+++ src/frontend/efiling-demo/src/components/page/home/Home.js
@@ -1,7 +1,46 @@
 import React from "react";
+import axios from "axios";
 import { Button } from "../../base/button/Button";
 
-export const generateUrl = () => {};
+const generateUrlBody = {
+  documentProperties: {
+    type: "string",
+    subType: "string",
+    submissionAccess: {
+      url: "string",
+      verb: "GET",
+      headers: {
+        additionalProp1: "string",
+        additionalProp2: "string",
+        additionalProp3: "string"
+      }
+    }
+  },
+  navigation: {
+    success: {
+      url: "string"
+    },
+    error: {
+      url: "string"
+    },
+    cancel: {
+      url: "string"
+    }
+  }
+};
+
+export const generateUrl = () => {
+  axios
+    .post(`/submission/generateUrl`, generateUrlBody)
+    .then(response => {
+      console.log(response);
+    })
+    .catch(() => {
+      throw new Error(
+        "An error occurred with generating the url. Please try again."
+      );
+    });
+};
 
 export default function App() {
   return (
