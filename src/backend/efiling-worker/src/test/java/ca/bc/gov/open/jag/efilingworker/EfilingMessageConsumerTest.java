50893362dc822259dcb36610295183d353a120bb
TayGov <Taylor.Clausen@gov.bc.ca>
2020-06-17 06:58:48 -0700

    Fixing test

--- src/backend/efiling-worker/src/test/java/ca/bc/gov/open/jag/efilingworker/EfilingMessageConsumerTest.java
+++ src/backend/efiling-worker/src/test/java/ca/bc/gov/open/jag/efilingworker/EfilingMessageConsumerTest.java
@@ -2,12 +2,42 @@ package ca.bc.gov.open.jag.efilingworker;
 
 import ca.bc.gov.open.jag.efilinglookupclient.MockCSOLookupServiceImpl;
 import ca.bc.gov.open.jag.efilingworker.service.MockDocumentStoreServiceImpl;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DisplayName("EfilingMessageConsumer Test Suite")
 public class EfilingMessageConsumerTest {
+    private static final String TEST = "TEST";
+    @InjectMocks
+    EfilingMessageConsumer sut;
+
+    @Mock
+    MockDocumentStoreServiceImpl mockDocumentStoreService;
+
+    @Mock
+    MockCSOLookupServiceImpl mockCSOLookupService;
+
+    @BeforeAll
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+        Mockito.when(mockDocumentStoreService.uploadFile(any())).thenReturn(TEST);
+    }
+
+    @DisplayName("CASE1: Test acceptMessage execution")
     @Test
     public void test() {
-        EfilingMessageConsumer efilingMessageConsumer = new EfilingMessageConsumer(new MockDocumentStoreServiceImpl(), new MockCSOLookupServiceImpl());
-        efilingMessageConsumer.acceptMessage("TEST");
+        sut.acceptMessage("TEST");
+        verify(mockDocumentStoreService, times(1)).uploadFile(any());
     }
 }
