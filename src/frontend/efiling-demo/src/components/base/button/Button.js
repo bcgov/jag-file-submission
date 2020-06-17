af9c6d2d59ff864b5e0db0d894e9515a590d316d
Shreyas Devalapurkar <shreyas.devalapurkar@gov.bc.ca>
2020-06-17 10:27:00 -0700

    remove unused classes

--- src/frontend/efiling-demo/src/components/base/button/Button.js
+++ src/frontend/efiling-demo/src/components/base/button/Button.js
@@ -3,12 +3,10 @@ import PropTypes from "prop-types";
 import "./Button.css";
 
 export const Button = ({ onClick, label }) => (
-  <div className="outline">
-    <div className="center-align">
-      <button className="efile-btn btn" onClick={onClick} type="button">
-        {label}
-      </button>
-    </div>
+  <div>
+    <button className="efile-btn btn" onClick={onClick} type="button">
+      {label}
+    </button>
   </div>
 );
 
