d9fc744c369545bbb758e9c08aef59a8f3d216b8
karthikmsqa <karthikmsqa@gmail.com>
2020-06-17 19:37:16 -0700

    Parameterize URI and resource values

--- tests/src/main/java/ca/bc/gov/open/jagefilingapi/qa/restgenerateurl/GenerateUrl.java
+++ tests/src/main/java/ca/bc/gov/open/jagefilingapi/qa/restgenerateurl/GenerateUrl.java
@@ -2,19 +2,27 @@ package ca.bc.gov.open.jagefilingapi.qa.restgenerateurl;
 
 import ca.bc.gov.open.jagefilingapi.qa.generateurlrestfile.*;
 import ca.bc.gov.open.jagefilingapi.qa.generateurlrestfile.Error;
+import ca.bc.gov.open.jagefilingapi.qa.util.ReadConfig;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class GenerateUrl {
 
-   public void generateEfilingUrl() throws JsonProcessingException {
-        baseURI = "http://localhost:8080";
+    ReadConfig readConfig;
+
+   public void generateEfilingUrl() throws IOException {
+       readConfig = new ReadConfig();
+
+       baseURI= readConfig.getBaseUri();
+       String resource = readConfig.getResource();
 
         given().log().all().header("Content-Type", "application/json")
-               .body(GenerateUrlPayload.generateUrlFinalPayload()).when().post("submission/generateUrl")
-               .then().log().all().assertThat().statusCode(200).body("efilingUrl", containsString("https://httpbin.org"));
+               .body(GenerateUrlPayload.generateUrlFinalPayload()).when().post(resource)
+               .then().log().all().assertThat().statusCode(200);
     }
 }
--- tests/src/test/java/ca/bc/gov/open/jagefilingapi/qa/tests/GenerateUrlTest.java
+++ tests/src/test/java/ca/bc/gov/open/jagefilingapi/qa/tests/GenerateUrlTest.java
@@ -4,11 +4,13 @@ import ca.bc.gov.open.jagefilingapi.qa.restgenerateurl.GenerateUrl;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.Test;
 
+import java.io.IOException;
+
 public class GenerateUrlTest {
     GenerateUrl generateUrl;
 
     @Test
-    public void verifyEfilingUrlIsGeneratedTest() throws JsonProcessingException {
+    public void verifyEfilingUrlIsGeneratedTest() throws IOException {
         generateUrl = new GenerateUrl();
 
         generateUrl.generateEfilingUrl();
