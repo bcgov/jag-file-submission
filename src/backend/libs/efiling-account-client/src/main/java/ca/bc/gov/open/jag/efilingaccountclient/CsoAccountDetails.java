f155a5e48088697994e4cf7fc459e4763bcc3291
alex <alex.joyeux@gov.bc.ca>
2020-06-18 16:43:54 -0700

    removing jsonCreator

--- src/backend/libs/efiling-account-client/src/main/java/ca/bc/gov/open/jag/efilingaccountclient/CsoAccountDetails.java
+++ src/backend/libs/efiling-account-client/src/main/java/ca/bc/gov/open/jag/efilingaccountclient/CsoAccountDetails.java
@@ -8,8 +8,7 @@ import java.util.List;
 
 public class CsoAccountDetails {
 
-
-    @JsonCreator
+    
     public CsoAccountDetails(
             @JsonProperty("accountId") String accountId,
             @JsonProperty("clientId") String clientId) {
