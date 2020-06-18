e36984b8b2aaa092c4885a6899a8d7ed144e29ef
Kyle Flood <Kyle.Flood@nttdata.com>
2020-06-18 14:01:53 -0700

    Comment on Service
    Comment on Service

--- src/backend/libs/efiling-account-client/src/main/java/ca/bc/gov/open/jag/efilingaccountclient/EfilingAccountService.java
+++ src/backend/libs/efiling-account-client/src/main/java/ca/bc/gov/open/jag/efilingaccountclient/EfilingAccountService.java
@@ -1,5 +1,8 @@
 package ca.bc.gov.open.jag.efilingaccountclient;
 
+/**
+ * Interface for getting CSO account details based on a user GUID
+ */
 public interface EfilingAccountService {
 
     CsoAccountDetails getAccountDetails(String userGuid);
