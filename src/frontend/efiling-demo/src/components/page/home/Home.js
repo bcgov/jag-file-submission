1cd7aef61d5b4fa207b32fbcff9a017a3443ac04
Shreyas Devalapurkar <shreyas.devalapurkar@gov.bc.ca>
2020-06-18 14:23:00 -0700

    create shared component app and use component within demo app

--- src/frontend/efiling-demo/src/components/page/home/Home.js
+++ src/frontend/efiling-demo/src/components/page/home/Home.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Button } from "../../base/button/Button";
+import { FooterComponent, HeaderComponent } from "shared-components";
+// import { TodaysDate } from 'shared-react-component-example';
 
 export const generateUrl = () => {};
 
 export default function App() {
   return (
     <div>
+      <HeaderComponent header={{ name: "Sauce" }} />
       <p>eFiling Demo Client</p>
       <Button onClick={generateUrl} label="With CSO Account" />
       <br />
       <Button onClick={generateUrl} label="Without CSO Account" />
+      <FooterComponent />
     </div>
   );
 }
---
+++ src/frontend/shared-components/src/index.js
@@ -0,0 +1,2 @@
+export { Footer as FooterComponent } from "./footer/Footer";
+export { Header as HeaderComponent } from "./header/Header";
