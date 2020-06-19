b17ca9d09296ad7c5c725ac892d42cecc030f348
Kyle Flood <Kyle.Flood@nttdata.com>
2020-06-19 09:38:01 -0700

    Moving back to real config
    Moving back to real config

--- src/backend/libs/efiling-lookup-client/src/main/java/ca/bc/gov/open/jag/efilinglookupclient/config/AutoConfiguration.java
+++ src/backend/libs/efiling-lookup-client/src/main/java/ca/bc/gov/open/jag/efilinglookupclient/config/AutoConfiguration.java
@@ -1,6 +1,6 @@
 package ca.bc.gov.open.jag.efilinglookupclient.config;
 
-import ca.bc.gov.open.jag.efilinglookupclient.DemoLookupServiceImpl;
+import ca.bc.gov.open.jag.efilinglookupclient.CSOLookupServiceImpl;
 import ca.bc.gov.open.jag.efilinglookupclient.EfilingLookupService;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -10,13 +10,31 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
 @Configuration
+@EnableConfigurationProperties(CSOLookupProperties.class)
 public class AutoConfiguration {
+
+    private final CSOLookupProperties csoLookupProperties;
+
+    public AutoConfiguration(CSOLookupProperties csoLookupProperties) {
+        this.csoLookupProperties = csoLookupProperties;
+    }
+
+    @Bean(name = "CSOLookupMarshaller")
+    public Jaxb2Marshaller marshaller() {
+
+        Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
+        jaxb2Marshaller.setContextPath("ca.bc.gov.ag.csows.lookups");
+        return jaxb2Marshaller;
+    }
 
     @Bean
     @ConditionalOnMissingBean(value = {EfilingLookupService.class})
-    public EfilingLookupService eFilingLookupService() {
+    public EfilingLookupService eFilingLookupService(@Qualifier("CSOLookupMarshaller") Jaxb2Marshaller jaxb2Marshaller) {
 
-        EfilingLookupService eFilingLookupService = new DemoLookupServiceImpl();
+        CSOLookupServiceImpl eFilingLookupService = new CSOLookupServiceImpl();
+        eFilingLookupService.setDefaultUri(csoLookupProperties.getFilingLookupSoapUri());
+        eFilingLookupService.setMarshaller(jaxb2Marshaller);
+        eFilingLookupService.setUnmarshaller(jaxb2Marshaller);
         return eFilingLookupService;
     }
 }
