4af97ccd1801726da2bb8992d85ba67679cc2c70
Shreyas Devalapurkar <shreyas.devalapurkar@gov.bc.ca>
2020-06-19 14:28:59 -0700

    add jest and storyshot tests

---
+++ src/frontend/shared-components/src/setupTests.js
@@ -0,0 +1,8 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import "regenerator-runtime/runtime";
+
+// to address Error: Not implemented: HTMLCanvasElement.prototype.getContext
+window.HTMLCanvasElement.prototype.getContext = () => {};
