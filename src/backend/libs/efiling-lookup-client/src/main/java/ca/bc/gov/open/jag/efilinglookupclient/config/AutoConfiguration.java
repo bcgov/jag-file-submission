bd403bf07c710f94b17feabc247a3eac9067706d
Kyle Flood <Kyle.Flood@nttdata.com>
2020-06-19 09:27:24 -0700

    Efiling LookupDemo Service
    Efiling LookupDemo Service

--- src/backend/libs/efiling-lookup-client/src/main/java/ca/bc/gov/open/jag/efilinglookupclient/config/AutoConfiguration.java
+++ src/backend/libs/efiling-lookup-client/src/main/java/ca/bc/gov/open/jag/efilinglookupclient/config/AutoConfiguration.java
@@ -1,6 +1,5 @@
 package ca.bc.gov.open.jag.efilinglookupclient.config;
 
-import ca.bc.gov.open.jag.efilinglookupclient.CSOLookupServiceImpl;
 import ca.bc.gov.open.jag.efilinglookupclient.DemoLookupServiceImpl;
 import ca.bc.gov.open.jag.efilinglookupclient.EfilingLookupService;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -11,31 +10,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
 @Configuration
-@EnableConfigurationProperties(CSOLookupProperties.class)
 public class AutoConfiguration {
-
-    private final CSOLookupProperties csoLookupProperties;
-
-    public AutoConfiguration(CSOLookupProperties csoLookupProperties) {
-        this.csoLookupProperties = csoLookupProperties;
-    }
-
-    @Bean(name = "CSOLookupMarshaller")
-    public Jaxb2Marshaller marshaller() {
-
-        Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
-        jaxb2Marshaller.setContextPath("ca.bc.gov.ag.csows.lookups");
-        return jaxb2Marshaller;
-    }
 
     @Bean
     @ConditionalOnMissingBean(value = {EfilingLookupService.class})
-    public EfilingLookupService eFilingLookupService(@Qualifier("CSOLookupMarshaller") Jaxb2Marshaller jaxb2Marshaller) {
+    public EfilingLookupService eFilingLookupService() {
 
         EfilingLookupService eFilingLookupService = new DemoLookupServiceImpl();
-//        eFilingLookupService.setDefaultUri(csoLookupProperties.getFilingLookupSoapUri());
-//        eFilingLookupService.setMarshaller(jaxb2Marshaller);
-//        eFilingLookupService.setUnmarshaller(jaxb2Marshaller);
         return eFilingLookupService;
     }
 }
