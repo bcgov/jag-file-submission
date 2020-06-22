23dc3f4498b8413481a5cdd46130e19971a72bed
TayGov <Taylor.Clausen@gov.bc.ca>
2020-06-22 07:14:09 -0700

    Cleanups some unused imports

--- src/backend/libs/efiling-lookup-client/src/main/java/ca/bc/gov/open/jag/efilinglookupclient/CSOLookupServiceImpl.java
+++ src/backend/libs/efiling-lookup-client/src/main/java/ca/bc/gov/open/jag/efilinglookupclient/CSOLookupServiceImpl.java
@@ -1,7 +1,5 @@
 package ca.bc.gov.open.jag.efilinglookupclient;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 
