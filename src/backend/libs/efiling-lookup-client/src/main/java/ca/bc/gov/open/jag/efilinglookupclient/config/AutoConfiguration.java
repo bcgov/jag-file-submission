8c060b9b0abffd2ecb3fe4fbde64e95a7f34e08d
Kyle Flood <Kyle.Flood@nttdata.com>
2020-06-22 10:03:45 -0700

    Lookup Soap Service
    Lookup Soap Service

--- src/backend/libs/efiling-lookup-client/src/main/java/ca/bc/gov/open/jag/efilinglookupclient/config/AutoConfiguration.java
+++ src/backend/libs/efiling-lookup-client/src/main/java/ca/bc/gov/open/jag/efilinglookupclient/config/AutoConfiguration.java
@@ -1,13 +1,9 @@
 package ca.bc.gov.open.jag.efilinglookupclient.config;
 
-import ca.bc.gov.open.jag.efilinglookupclient.CSOLookupServiceImpl;
-import ca.bc.gov.open.jag.efilinglookupclient.EfilingLookupService;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import ca.bc.gov.ag.csows.LookupsLookupFacade;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
 @Configuration
 @EnableConfigurationProperties(CSOLookupProperties.class)
@@ -19,22 +15,9 @@ public class AutoConfiguration {
         this.csoLookupProperties = csoLookupProperties;
     }
 
-    @Bean(name = "CSOLookupMarshaller")
-    public Jaxb2Marshaller marshaller() {
-
-        Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
-        jaxb2Marshaller.setContextPath("ca.bc.gov.ag.csows.lookups");
-        return jaxb2Marshaller;
-    }
-
     @Bean
-    @ConditionalOnMissingBean(value = {EfilingLookupService.class})
-    public EfilingLookupService eFilingLookupService(@Qualifier("CSOLookupMarshaller") Jaxb2Marshaller jaxb2Marshaller) {
-
-        CSOLookupServiceImpl eFilingLookupService = new CSOLookupServiceImpl();
-        eFilingLookupService.setDefaultUri(csoLookupProperties.getFilingLookupSoapUri());
-        eFilingLookupService.setMarshaller(jaxb2Marshaller);
-        eFilingLookupService.setUnmarshaller(jaxb2Marshaller);
-        return eFilingLookupService;
+    public LookupsLookupFacade eFilingLookupService() {
+        LookupsLookupFacade lookupsLookupFacade = new LookupsLookupFacade();
+        return lookupsLookupFacade;
     }
 }
