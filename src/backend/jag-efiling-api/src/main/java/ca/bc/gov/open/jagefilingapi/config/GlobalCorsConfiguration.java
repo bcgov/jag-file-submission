730caef02ffcf97a8a9ba85e7185d4c25dd329dc
Shreyas Devalapurkar <shreyas.devalapurkar@gov.bc.ca>
2020-06-22 11:06:26 -0700

    add global cors configuration

---
+++ src/backend/jag-efiling-api/src/main/java/ca/bc/gov/open/jagefilingapi/config/GlobalCorsConfiguration.java
@@ -0,0 +1,20 @@
+package ca.bc.gov.open.jagefilingapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class GlobalCorsConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
+            }
+        };
+    }
+}
