1e163d564a7c171dd7dcb09ccbc1dcd75a673360
Shreyas Devalapurkar <shreyas.devalapurkar@gov.bc.ca>
2020-06-23 11:16:27 -0700

    get async storyshots working!

--- src/frontend/efiling-frontend/src/components/page/home/Home.stories.js
+++ src/frontend/efiling-frontend/src/components/page/home/Home.stories.js
@@ -6,6 +6,8 @@ import MockAdapter from "axios-mock-adapter";
 
 import Home from "./Home";
 
+console.error = () => {}; // for async storyshot errors, due to lack of support
+
 export default {
   title: "Home",
   component: Home
@@ -15,32 +17,58 @@ const header = {
   name: "eFiling Demo Client",
   history: createMemoryHistory()
 };
-
 const page = { header };
+
 const submissionId = "abc123";
+const mock = new MockAdapter(axios);
+const apiRequest = `/submission/${submissionId}/userDetail`;
 
 const LoaderStateData = props => {
-  const mock = new MockAdapter(axios);
-  const apiRequest = `/submission/${submissionId}/userDetail`;
-
   mock.onGet(apiRequest).reply(400);
-
   return props.children({ page });
 };
 
+const AccountExistsStateData = props => {
+  mock.onGet(apiRequest).reply(200, { csoAccountExists: true });
+  return props.children({ page });
+};
+
+const NoAccountExistsStateData = props => {
+  mock.onGet(apiRequest).reply(200, { csoAccountExists: false });
+  return props.children({ page });
+};
+
+const homeComponent = data => (
+  <MemoryRouter initialEntries={[`?submissionId=${submissionId}`]}>
+    <Home page={data.page} />
+  </MemoryRouter>
+);
+
 const loaderComponent = (
-  <LoaderStateData>
-    {data => (
-      <MemoryRouter initialEntries={[`/?submissionId=${submissionId}`]}>
-        <Home page={data.page} />
-      </MemoryRouter>
-    )}
-  </LoaderStateData>
+  <LoaderStateData>{data => homeComponent(data)}</LoaderStateData>
+);
+
+const accountExistsComponent = (
+  <AccountExistsStateData>{data => homeComponent(data)}</AccountExistsStateData>
+);
+
+const noAccountExistsComponent = (
+  <NoAccountExistsStateData>
+    {data => homeComponent(data)}
+  </NoAccountExistsStateData>
 );
 
 export const Loader = () => loaderComponent;
 
 export const LoaderMobile = () => loaderComponent;
+
+export const AccountExists = () => accountExistsComponent;
+
+export const AccountExistsMobile = () => accountExistsComponent;
+
+export const NoAccountExists = () => noAccountExistsComponent;
+
+export const NoAccountExistsMobile = () => noAccountExistsComponent;
 
 const mobileViewport = {
   parameters: {
@@ -51,3 +79,5 @@ const mobileViewport = {
 };
 
 LoaderMobile.story = mobileViewport;
+AccountExistsMobile.story = mobileViewport;
+NoAccountExistsMobile.story = mobileViewport;
--- src/frontend/efiling-frontend/src/storyshots.test.js
+++ src/frontend/efiling-frontend/src/storyshots.test.js
@@ -1,5 +1,40 @@
-import initStoryshots from "@storybook/addon-storyshots";
+import initStoryshots, {
+  Stories2SnapsConverter
+} from "@storybook/addon-storyshots";
+import { act, create } from "react-test-renderer";
+
+const wait = () =>
+  act(
+    () =>
+      new Promise(resolve => {
+        setTimeout(resolve, 10);
+      })
+  );
+
+const converter = new Stories2SnapsConverter();
+
+const runTest = async (story, context) => {
+  const filename = converter.getSnapshotFileName(context);
+
+  if (!filename) {
+    return;
+  }
+
+  const storyElement = story.render();
+  let tree;
+  act(() => {
+    tree = create(storyElement);
+  });
+
+  await wait();
+  expect(tree.toJSON()).toMatchSpecificSnapshot(filename);
+
+  tree.unmount();
+};
 
 initStoryshots({
-  /* configuration options */
+  asyncJest: true,
+  test: ({ story, context, done }) => {
+    runTest(story, context).then(done);
+  }
 });
