aff743ffe2cacb6d590109d85d88f863020580fa
adodge-bcg <alan.dodge@gov.bc.ca>
2020-06-23 13:11:20 -0700

    Update babel.config.js

--- src/frontend/shared-components/babel.config.js
+++ src/frontend/shared-components/babel.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react"]
+  presets: ["@babel/preset-env", "@babel/preset-react"],
+  plugins: ["transform-export-extensions"],
+  only: ["./**/*.js", "node_modules/jest-runtime"]
 };
