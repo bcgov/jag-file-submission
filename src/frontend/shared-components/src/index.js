6616f1ea8a59fcf78eac2c6547f6d66479ea9e21
Shreyas Devalapurkar <shreyas.devalapurkar@gov.bc.ca>
2020-06-23 09:09:52 -0700

    export button from shared-components

--- src/frontend/shared-components/src/index.js
+++ src/frontend/shared-components/src/index.js
@@ -3,4 +3,5 @@ import Header from "./header/Header";
 export { Footer } from "./footer/Footer";
 export { Loader } from "./loader/Loader";
 export { Input } from "./input/Input";
+export { Button } from "./button/Button";
 export default Header;
