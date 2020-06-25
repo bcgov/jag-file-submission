f55a5c3c1929a9cd733d8d23d78c77b678fc1d01
Shreyas Devalapurkar <shreyas.devalapurkar@gov.bc.ca>
2020-06-25 08:40:53 -0700

    remove unused packages

--- src/frontend/shared-components/src/storyshots.test.js
+++ src/frontend/shared-components/src/storyshots.test.js
@@ -1,10 +1,6 @@
 import initStoryshots from "@storybook/addon-storyshots";
-import { mount } from "enzyme";
-import { createSerializer } from "enzyme-to-json";
 
 initStoryshots({
   /* configuration options */
-  // renderer: mount,
-  // snapshotSerializers: [createSerializer()]
   storyKindRegex: /^((?!.*?ConfirmationPopup).)*$/
 });
