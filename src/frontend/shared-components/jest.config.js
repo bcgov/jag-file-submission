f707c7894eef5cda21561f594b397a04c8e880cf
Shreyas Devalapurkar <shreyas.devalapurkar@gov.bc.ca>
2020-06-26 11:58:57 -0700

    updates

--- src/frontend/shared-components/jest.config.js
+++ src/frontend/shared-components/jest.config.js
@@ -19,6 +19,6 @@ module.exports = {
   verbose: true,
   testResultsProcessor: "jest-sonar-reporter",
   collectCoverage: false,
-  coverageReporters: ["text", "lcov"],
+  coverageReporters: ["text", ["lcov", { projectRoot: "../../../" }]],
   coverageDirectory: "coverage"
 };
