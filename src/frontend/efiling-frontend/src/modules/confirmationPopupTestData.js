c15b0e4bcfee354bcc35ac5de168b666562a7aa7
Shreyas <shreyas.devalapurkar@gov.bc.ca>
2020-06-29 10:12:02 -0700

    fix issue with code climate

--- src/frontend/efiling-frontend/src/modules/confirmationPopupTestData.js
+++ src/frontend/efiling-frontend/src/modules/confirmationPopupTestData.js
@@ -1,35 +1,35 @@
 import React from "react";
 
+const mainButton = {
+  mainLabel: "Cancel",
+  mainStyling: "normal-white btn"
+};
+
+const confirmButton = {
+  confirmLabel: "Yes, cancel",
+  confirmStyling: "normal-blue btn consistent-width"
+};
+
+const cancelButton = {
+  cancelLabel: "No, dont cancel",
+  cancelStyling: "normal-white btn consistent-width"
+};
+
+const handleClose = () => {};
+const handleShow = () => {};
+
+const body = () => (
+  <>
+    <p>Your files will not be submitted.</p>
+    <p>
+      You will be returned to:
+      <br />
+      <b>Original</b> website
+    </p>
+  </>
+);
+
 export function getTestData() {
-  const mainButton = {
-    mainLabel: "Cancel",
-    mainStyling: "normal-white btn"
-  };
-
-  const confirmButton = {
-    confirmLabel: "Yes, cancel",
-    confirmStyling: "normal-blue btn consistent-width"
-  };
-
-  const cancelButton = {
-    cancelLabel: "No, dont cancel",
-    cancelStyling: "normal-white btn consistent-width"
-  };
-
-  const handleClose = () => {};
-  const handleShow = () => {};
-
-  const body = () => (
-    <>
-      <p>Your files will not be submitted.</p>
-      <p>
-        You will be returned to:
-        <br />
-        <b>Original</b> website
-      </p>
-    </>
-  );
-
   const modal = {
     show: true,
     handleShow,
