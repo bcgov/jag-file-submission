4fd3d07eb83688510be54860d7644d274ecd4458
Alan Dodge <alan.dodge@gov.bc.ca>
2020-06-30 15:39:05 -0700

    Refactored to use testBasicSnapshot, updated snapshots

--- src/frontend/shared-components/src/components/table/Table.test.js
+++ src/frontend/shared-components/src/components/table/Table.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import testBasicSnapshot from "../../TestHelper";
 
 import Table from "./Table";
 import { getTableTestData } from "../../modules/tableTestData";
@@ -8,14 +8,10 @@ const tableData = getTableTestData();
 
 describe("Table Component", () => {
   test("Matches the default snapshot", () => {
-    const { asFragment } = render(<Table table={tableData} />);
-    expect(asFragment).toMatchSnapshot();
+    testBasicSnapshot(<Table table={tableData} />);
   });
 
   test("Matches the blue striped snapshot", () => {
-    const { asFragment } = render(
-      <Table table={{ ...tableData, style: "blue-stripe" }} />
-    );
-    expect(asFragment).toMatchSnapshot();
+    testBasicSnapshot(<Table table={{ ...tableData, style: "blue-stripe" }} />);
   });
 });
