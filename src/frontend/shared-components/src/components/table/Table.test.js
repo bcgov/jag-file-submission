fbb7785991e52619b54cfecea750daa54624071c
Alan Dodge <alan.dodge@gov.bc.ca>
2020-06-30 14:20:29 -0700

    Added blue striped snapshot test

--- src/frontend/shared-components/src/components/table/Table.test.js
+++ src/frontend/shared-components/src/components/table/Table.test.js
@@ -3,24 +3,31 @@ import { render } from "@testing-library/react";
 
 import Table from "./Table";
 
+const tableElements = [
+  { name: "BCeID", value: "bobross42" },
+  {
+    name: "Last Name",
+    value: "Ross"
+  },
+  { name: "First Name", value: "Bob" },
+  { name: "Email Address", value: "bob.ross@pbs.com" }
+];
+
+const table = {
+  header: "BCeID Info",
+  tableElements
+};
+
 describe("Table Component", () => {
-  test("Matches the snapshot", () => {
-    const tableElements = [
-      { name: "BCeID", value: "bobross42" },
-      {
-        name: "Last Name",
-        value: "Ross"
-      },
-      { name: "First Name", value: "Bob" },
-      { name: "Email Address", value: "bob.ross@pbs.com" }
-    ];
-
-    const table = {
-      header: "BCeID Info",
-      tableElements
-    };
-
+  test("Matches the default snapshot", () => {
     const { asFragment } = render(<Table table={table} />);
+    expect(asFragment).toMatchSnapshot();
+  });
+
+  test("Matches the blue striped snapshot", () => {
+    const { asFragment } = render(
+      <Table table={{ ...table, tableStyle: "blueStripe" }} />
+    );
     expect(asFragment).toMatchSnapshot();
   });
 });
