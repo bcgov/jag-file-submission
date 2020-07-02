06a95785777b75e6e652c7a13f03507af88eebb2
Shreyas <shreyas.devalapurkar@gov.bc.ca>
2020-07-02 10:58:50 -0700

    fix lint

--- src/frontend/efiling-frontend/src/components/page/cso-account/CSOAccount.js
+++ src/frontend/efiling-frontend/src/components/page/cso-account/CSOAccount.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
 import ConfirmationPopup, { TermsOfUse, Button } from "shared-components";
 import { getContent } from "../../../modules/csoAccountAgreementContent";
 
