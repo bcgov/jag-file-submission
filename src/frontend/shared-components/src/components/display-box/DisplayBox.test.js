4568b40ee0f8d6ea12be3afe39dfcf2bb3537786
Alan Dodge <alan.dodge@gov.bc.ca>
2020-07-02 13:38:39 -0700

    Added tests

---
+++ src/frontend/shared-components/src/components/display-box/DisplayBox.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { MdPerson } from "react-icons/md";
+
+import testBasicSnapshot from "../../TestHelper";
+import DisplayBox from "./DisplayBox";
+import { getTestTable } from "../../modules/displayBoxTestData";
+
+const tableData = getTestTable();
+const icon = <MdPerson size={32} />;
+
+describe("Table Component", () => {
+  test("Matches the without icon snapshot", () => {
+    testBasicSnapshot(<DisplayBox element={tableData} />);
+  });
+
+  test("Matches the with icon snapshot", () => {
+    testBasicSnapshot(<DisplayBox icon element={tableData} />);
+  });
+
+  test("Matches the blue-background snapshot", () => {
+    testBasicSnapshot(
+      <DisplayBox styling={"blue-background"} icon element={tableData} />
+    );
+  });
+});
