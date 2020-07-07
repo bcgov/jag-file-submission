804c7874959d377e41f4a809f412968e32cdb141
Shreyas <shreyas.devalapurkar@gov.bc.ca>
2020-07-07 14:25:26 -0700

    integrate sidecard into pages and fix styling

--- src/frontend/efiling-frontend/src/components/page/cso-account/CSOAccount.js
+++ src/frontend/efiling-frontend/src/components/page/cso-account/CSOAccount.js
@@ -6,7 +6,8 @@ import ConfirmationPopup, {
   TermsOfUse,
   Button,
   DisplayBox,
-  Table
+  Table,
+  Sidecard
 } from "shared-components";
 import { getContent } from "../../../modules/csoAccountAgreementContent";
 import { translateApplicantInfo } from "../../../modules/translateApplicantInfo";
@@ -16,6 +17,28 @@ const continueButton = {
   label: "Create CSO Account",
   styling: "normal-blue btn",
   onClick: () => {}
+};
+
+const sideCard = {
+  heading: "About E-File Submission",
+  content: [
+    <p key="aboutCso">
+      E-File submission is a service to help you securely and electronically
+      file documents with the Government of British Columbia Court Services
+      Online (CSO).&nbsp;
+      <a
+        href="https://justice.gov.bc.ca/cso/about/index.do"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn more about CSO
+      </a>
+      .
+    </p>
+  ],
+  type: "bluegrey",
+  isWide: true,
+  icon: <MdPerson className="side-card-icon" />
 };
 
 const content = getContent();
@@ -42,57 +65,44 @@ export default function CSOAccount({ confirmationPopup, applicantInfo }) {
   );
 
   return (
-    <>
-      <div className="non-printable">
-        <p>
-          E-File Submission is a service to help you securely and electronically
-          file documents with the Government of British Columbia Court Services
-          Online (CSO).&nbsp;
-          <a
-            href="https://justice.gov.bc.ca/cso/about/index.do"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn more about CSO
-          </a>
-          .
-        </p>
-        <p>You need a CSO account to use E-File Submission.</p>
+    <div className="page">
+      <div className="content col-md-8">
+        <div className="non-printable">
+          <h2>Create a Court Services Online (CSO) Account</h2>
+          <p>
+            The following information will be used to create your CSO account.
+          </p>
+          <DisplayBox icon={icon} element={applicantTable} />
+          <br />
+        </div>
 
-        <br />
+        <TermsOfUse
+          acceptTerms={() => acceptTerms(!termsAccepted)}
+          content={content}
+          heading="Electronic Service Agreement"
+          confirmText="I accept the Service Agreement"
+        />
 
-        <h2>Create a Court Services Online (CSO) Account</h2>
-        <p>
-          The following information will be used to create your CSO account.
-        </p>
-
-        <DisplayBox icon={icon} element={applicantTable} />
-
-        <br />
+        <section className="non-printable buttons pt-4">
+          <ConfirmationPopup
+            modal={confirmationPopup.modal}
+            mainButton={confirmationPopup.mainButton}
+            confirmButton={confirmationPopup.confirmButton}
+            cancelButton={confirmationPopup.cancelButton}
+          />
+          <Button
+            label={continueButton.label}
+            onClick={continueButton.onClick}
+            styling={continueButton.styling}
+            disabled={!continueBtnEnabled}
+          />
+        </section>
       </div>
 
-      <TermsOfUse
-        acceptTerms={() => acceptTerms(!termsAccepted)}
-        content={content}
-        heading="Electronic Service Agreement"
-        confirmText="I accept the Service Agreement"
-      />
-
-      <section className="non-printable buttons pt-4">
-        <ConfirmationPopup
-          modal={confirmationPopup.modal}
-          mainButton={confirmationPopup.mainButton}
-          confirmButton={confirmationPopup.confirmButton}
-          cancelButton={confirmationPopup.cancelButton}
-        />
-        <Button
-          label={continueButton.label}
-          onClick={continueButton.onClick}
-          styling={continueButton.styling}
-          disabled={!continueBtnEnabled}
-        />
-      </section>
-    </>
+      <div className="sidecard">
+        <Sidecard sideCard={sideCard} />
+      </div>
+    </div>
   );
 }
 
