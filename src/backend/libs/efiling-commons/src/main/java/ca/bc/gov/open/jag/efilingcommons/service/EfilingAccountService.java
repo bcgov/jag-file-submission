9102d42411b651cb4ac271973324bc08fba312c6
alex <alex.joyeux@gov.bc.ca>
2020-07-09 10:12:26 -0700

    added javadoc

--- src/backend/libs/efiling-commons/src/main/java/ca/bc/gov/open/jag/efilingcommons/service/EfilingAccountService.java
+++ src/backend/libs/efiling-commons/src/main/java/ca/bc/gov/open/jag/efilingcommons/service/EfilingAccountService.java
@@ -6,12 +6,23 @@ import ca.bc.gov.open.jag.efilingcommons.model.CreateAccountRequest;
 import java.util.UUID;
 
 /**
- * Interface for getting CSO account details based on a user GUID
+ * Interface for getting account details based on a user GUID
  */
 public interface EfilingAccountService {
 
+    /**
+     * Get the account details
+     * @param userGuid
+     * @param bceidAccountType
+     * @return
+     */
     AccountDetails getAccountDetails(UUID userGuid, String bceidAccountType);
 
+    /**
+     * Creates a new account
+     * @param createAccountRequest
+     * @return
+     */
     AccountDetails createAccount(CreateAccountRequest createAccountRequest);
 
 }
