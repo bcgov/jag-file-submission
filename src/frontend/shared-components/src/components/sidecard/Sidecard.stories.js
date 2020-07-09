4d0b450cd89b350f9e256e0bd7235e08b9480cc3
Shreyas <shreyas.devalapurkar@gov.bc.ca>
2020-07-09 09:23:17 -0700

    add sidecard docs

--- src/frontend/shared-components/src/components/sidecard/Sidecard.stories.js
+++ src/frontend/shared-components/src/components/sidecard/Sidecard.stories.js
@@ -4,32 +4,21 @@ import { Sidecard } from "./Sidecard";
 
 export default {
   title: "Sidecard",
-  component: Sidecard
+  component: Sidecard,
+  includeStories: []
 };
 
 const sideCard = {
-  heading: "About E-File Submission",
-  content: [
-    "E-File submission allows you to securely upload and electronically file documents."
-  ],
+  heading: "Heading",
+  content: ["This is where I put my content for the sidecard"],
   type: "blue"
 };
 
 export const Blue = () => <Sidecard sideCard={sideCard} />;
 
-export const BlueMobile = () => <Sidecard sideCard={sideCard} />;
-
 export const Grey = () => <Sidecard sideCard={{ ...sideCard, type: "grey" }} />;
 
-export const GreyMobile = () => (
-  <Sidecard sideCard={{ ...sideCard, type: "grey" }} />
-);
-
 export const BlueGrey = () => (
-  <Sidecard sideCard={{ ...sideCard, type: "bluegrey" }} />
-);
-
-export const BlueGreyMobile = () => (
   <Sidecard sideCard={{ ...sideCard, type: "bluegrey" }} />
 );
 
@@ -61,7 +50,4 @@ const mobileViewport = {
   }
 };
 
-BlueMobile.story = mobileViewport;
-GreyMobile.story = mobileViewport;
-BlueGreyMobile.story = mobileViewport;
 WithIconMobile.story = mobileViewport;
