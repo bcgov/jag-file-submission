e59496f806204c54e0e3fb8b91cf1eeae0fd8922
Shreyas <shreyas.devalapurkar@gov.bc.ca>
2020-07-13 12:54:41 -0700

    add id to textarea, fix lint

--- src/frontend/shared-components/src/components/textarea/Textarea.js
+++ src/frontend/shared-components/src/components/textarea/Textarea.js
@@ -3,25 +3,27 @@ import PropTypes from "prop-types";
 
 import "./Textarea.css";
 
-export const Textarea = ({ label, onChange }) => {
+export const Textarea = ({ id, label, onChange }) => {
   return (
     <>
       {label && (
         <div className="text-label">
-          <label>{label}</label>
+          <label htmlFor={id}>{label}</label>
         </div>
       )}
       <textarea
+        id={id}
         className="text-input"
         rows="8"
         cols="60"
         onChange={event => onChange(event.target.value)}
-      ></textarea>
+      />
     </>
   );
 };
 
 Textarea.propTypes = {
+  id: PropTypes.string.isRequired,
   label: PropTypes.string,
   onChange: PropTypes.func.isRequired
 };
