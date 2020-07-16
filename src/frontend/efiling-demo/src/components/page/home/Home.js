51a949c3fc4342bc4c63c88b064c4b75822146ad
Shreyas <shreyas.devalapurkar@gov.bc.ca>
2020-07-15 17:16:28 -0700

    testing lint, should trigger

--- src/frontend/efiling-demo/src/components/page/home/Home.js
+++ src/frontend/efiling-demo/src/components/page/home/Home.js
@@ -88,6 +88,8 @@ const generatePackageData = files => {
 export const eFilePackage = (files, accountGuid, setErrorExists) => {
   const { formData, updatedUrlBody } = generatePackageData(files);
 
+  console.log("test lint");
+
   axios
     .post("/submission/documents", formData, {
       headers: {
