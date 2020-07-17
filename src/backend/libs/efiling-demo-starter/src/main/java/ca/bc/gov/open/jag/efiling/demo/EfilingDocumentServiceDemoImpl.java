f33d850639efe6ee000c4720822b16db2b20b040
TayGov <47829178+TayGov@users.noreply.github.com>
2020-07-17 10:50:14 -0700

    Update src/backend/libs/efiling-demo-starter/src/main/java/ca/bc/gov/open/jag/efiling/demo/EfilingDocumentServiceDemoImpl.java
    Co-authored-by: alexjoybc <51387119+alexjoybc@users.noreply.github.com>

--- src/backend/libs/efiling-demo-starter/src/main/java/ca/bc/gov/open/jag/efiling/demo/EfilingDocumentServiceDemoImpl.java
+++ src/backend/libs/efiling-demo-starter/src/main/java/ca/bc/gov/open/jag/efiling/demo/EfilingDocumentServiceDemoImpl.java
@@ -5,7 +5,7 @@ import ca.bc.gov.open.jag.efilingcommons.service.EfilingDocumentService;
 
 public class EfilingDocumentServiceDemoImpl implements EfilingDocumentService {
     @Override
-    public DocumentDetails getDocumentDetails(String serviceId) {
+    public DocumentDetails getDocumentDetails(String documentType) {
         return new DocumentDetails("This is a doc");
     }
 }
