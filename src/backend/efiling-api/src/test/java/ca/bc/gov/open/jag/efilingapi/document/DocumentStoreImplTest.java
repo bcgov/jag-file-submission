d2bcf660a8b085270890b403d9711370e491dc58
alex <alex.joyeux@gov.bc.ca>
2020-07-20 09:55:54 -0700

    Adding tests for coverage

--- src/backend/efiling-api/src/test/java/ca/bc/gov/open/jag/efilingapi/document/DocumentStoreImplTest.java
+++ src/backend/efiling-api/src/test/java/ca/bc/gov/open/jag/efilingapi/document/DocumentStoreImplTest.java
@@ -1,15 +1,20 @@
 package ca.bc.gov.open.jag.efilingapi.document;
 
+import ca.bc.gov.open.jag.efilingcommons.model.DocumentDetails;
 import ca.bc.gov.open.jag.efilingcommons.service.EfilingDocumentService;
 import org.junit.jupiter.api.*;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+
+import java.math.BigDecimal;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @DisplayName("DocumentStoreImpl test suite")
 public class DocumentStoreImplTest {
 
-    public static final String DUMMY_CONTENT = "test";
+    private static final String DUMMY_CONTENT = "test";
+    private static final String DESCRIPTION = "description";
     private DocumentStoreImpl sut;
 
     @Mock
@@ -18,11 +23,18 @@ public class DocumentStoreImplTest {
     @BeforeAll
     public void setUp() {
         MockitoAnnotations.initMocks(this);
+
+        DocumentDetails docummentDetails = new DocumentDetails(DESCRIPTION, BigDecimal.TEN);
+
+        Mockito
+                .when(efilingDocumentServiceMock.getDocumentDetails(Mockito.anyString(), Mockito.anyString(), Mockito.anyString()))
+                .thenReturn(docummentDetails);
+
         sut = new DocumentStoreImpl(efilingDocumentServiceMock);
     }
 
     @Test
-    @DisplayName("OK: should return byte array")
+    @DisplayName("OK: put document should return byte array")
     public void withoutCacheShouldReturnIt() {
 
         byte[] actual = sut.put("id", DUMMY_CONTENT.getBytes());
@@ -31,10 +43,20 @@ public class DocumentStoreImplTest {
     }
 
     @Test
-    @DisplayName("OK: should return null")
+    @DisplayName("OK: get document by Id should return null")
     public void withoutCacheShouldReturnNull() {
-
         Assertions.assertNull(sut.get("id"));
+    }
+
+    @Test
+    @DisplayName("OK: get document details should cache result")
+    public void withCourtLevelCourtClassDocumentTypeShouldReturnDocumentDetails() {
+
+
+        DocumentDetails actual = sut.getDocumentDetails("courtLevel", "courtClass", "documentType");
+
+        Assertions.assertEquals(DESCRIPTION, actual.getDescription());
+        Assertions.assertEquals(BigDecimal.TEN, actual.getStatutoryFeeAmount());
 
     }
 
