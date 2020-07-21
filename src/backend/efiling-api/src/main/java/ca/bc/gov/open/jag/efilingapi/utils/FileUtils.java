abb36647991030c708c63509dce6a9b495f680ce
alex <alex.joyeux@gov.bc.ca>
2020-07-21 11:38:48 -0700

    fix PR feedback

--- src/backend/efiling-api/src/main/java/ca/bc/gov/open/jag/efilingapi/utils/FileUtils.java
+++ src/backend/efiling-api/src/main/java/ca/bc/gov/open/jag/efilingapi/utils/FileUtils.java
@@ -5,7 +5,13 @@ import java.net.URLConnection;
 
 public class FileUtils {
 
+    private FileUtils() {}
 
+    /**
+     * Guess the mime type from filename
+     * @param fileName
+     * @return
+     */
     public static String guessContentTypeFromName(String fileName){
         File file = new File(fileName);
         return URLConnection.guessContentTypeFromName(file.getName());
