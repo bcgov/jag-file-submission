0a6c3ea8833518369614d1c22e908bc449a6c5d4
Shreyas <shreyas.devalapurkar@gov.bc.ca>
2020-07-27 13:20:42 -0700

    temp changes for showing each file with radio buttons and dropdown

--- src/frontend/efiling-frontend/src/components/page/upload/Upload.js
+++ src/frontend/efiling-frontend/src/components/page/upload/Upload.js
@@ -2,12 +2,108 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { useDropzone } from "react-dropzone";
-import { Sidecard, Button } from "shared-components";
+import { MdDescription } from "react-icons/md";
+import {
+  Sidecard,
+  Button,
+  DisplayBox,
+  Table,
+  Dropdown,
+  Radio
+} from "shared-components";
 import { getSidecardData } from "../../../modules/sidecardData";
 import { propTypes } from "../../../types/propTypes";
 
 import "./Upload.css";
 import PackageConfirmation from "../package-confirmation/PackageConfirmation";
+
+const icon = (
+  <div style={{ color: "rgb(252, 186, 25)" }}>
+    <MdDescription size={32} />
+  </div>
+);
+
+const generateTable = () => {
+  return [
+    {
+      name: (
+        <div className="center-alignment fill-space">
+          {icon}
+          <span className="minor-margin-left">jhsgdhjsgjh</span>
+        </div>
+      ),
+      value: (
+        <>
+          <div className="table-value">
+            <Dropdown
+              label="Description:"
+              items={items}
+              onSelect={val => console.log(val)}
+            />
+          </div>
+          <br />
+        </>
+      ),
+      verticalMiddle: false
+    },
+    {
+      name: (
+        <div className="major-padding-left">Is this document an amendment?</div>
+      ),
+      value: (
+        <div className="table-value">
+          <div className="minor-margin-right">
+            <Radio
+              id="no"
+              name="amendment"
+              label="No"
+              onSelect={val => console.log(val)}
+            />
+          </div>
+          <Radio
+            id="yes"
+            name="amendment"
+            label="Yes"
+            onSelect={val => console.log(val)}
+          />
+        </div>
+      )
+    },
+    {
+      name: (
+        <div className="major-padding-left">
+          Is this document that needs to go to supreme court scheduling aln
+          dodge man?
+        </div>
+      ),
+      value: (
+        <div className="table-value">
+          <div className="minor-margin-right">
+            <Radio
+              id="no"
+              name="amendment"
+              label="No"
+              onSelect={val => console.log(val)}
+            />
+          </div>
+          <Radio
+            id="yes"
+            name="amendment"
+            label="Yes"
+            onSelect={val => console.log(val)}
+          />
+        </div>
+      )
+    }
+  ];
+};
+
+const items = [
+  "Select document description",
+  "Option 2",
+  "Option 3",
+  "Option 4"
+];
 
 export default function Upload({
   upload: { confirmationPopup, submissionId }
@@ -51,6 +147,14 @@ export default function Upload({
         {acceptedFiles.length > 0 && (
           <>
             <h2>Select Description for each uploaded document to continue</h2>
+            <br />
+            <div>
+              <DisplayBox
+                styling="border-background"
+                element={<Table elements={generateTable()} />}
+              />
+              <br />
+            </div>
           </>
         )}
         <section className="buttons pt-2">
