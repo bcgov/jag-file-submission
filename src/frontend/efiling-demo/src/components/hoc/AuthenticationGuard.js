826247ae1000ebaa3ddbcd230dd88f661a805da2
Shreyas <shreyas.devalapurkar@gov.bc.ca>
2020-07-28 14:11:20 -0700

    add docker compose env vars and setup proper realm, url and clientid in auth guard

--- src/frontend/efiling-demo/src/components/hoc/AuthenticationGuard.js
+++ src/frontend/efiling-demo/src/components/hoc/AuthenticationGuard.js
@@ -5,7 +5,6 @@ import Home from "../page/home/Home";
 let url;
 let realm;
 let clientId;
-let accessRole;
 
 if (process.env.REACT_APP_KEYCLOAK_URL)
   url = process.env.REACT_APP_KEYCLOAK_URL;
@@ -16,12 +15,9 @@ if (process.env.REACT_APP_KEYCLOAK_REALM)
 if (process.env.REACT_APP_KEYCLOAK_CLIENT_ID)
   clientId = process.env.REACT_APP_KEYCLOAK_CLIENT_ID;
 
-if (process.env.REACT_APP_KEYCLOAK_ACCESS_ROLE)
-  accessRole = process.env.REACT_APP_KEYCLOAK_ACCESS_ROLE;
-
 const KEYCLOAK = {
-  url,
   realm,
+  url,
   clientId,
 };
 
@@ -42,20 +38,9 @@ export default function AuthenticationGuard({ page: { header } }) {
       })
       .success(() => {
         keycloak.loadUserInfo().success();
-        console.log(keycloak);
-        if (
-          accessRole &&
-          keycloak.tokenParsed.realm_access.roles.indexOf(accessRole) !== -1
-        ) {
-          localStorage.setItem("jwt", keycloak.token);
-          setAuthedKeycloak(keycloak);
-        } else {
-          keycloak.clearToken();
-          localStorage.clear();
-          alert(
-            "Authenticated but not Authorized, request access from your portal administrator"
-          );
-        }
+
+        localStorage.setItem("jwt", keycloak.token);
+        setAuthedKeycloak(keycloak);
       });
   }
 
@@ -66,7 +51,7 @@ export default function AuthenticationGuard({ page: { header } }) {
   return (
     <>
       {authedKeycloak && <Home page={{ header }} />}
-      {!authedKeycloak && <p>Unauthenticated!</p>}
+      {!authedKeycloak && null}
     </>
   );
 }
