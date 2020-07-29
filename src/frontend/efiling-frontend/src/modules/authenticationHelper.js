823cfe0835bb50479e1739b2c28d776f0a038ef9
Shreyas <shreyas.devalapurkar@gov.bc.ca>
2020-07-29 15:19:23 -0700

    cleanup

--- src/frontend/efiling-frontend/src/modules/authenticationHelper.js
+++ src/frontend/efiling-frontend/src/modules/authenticationHelper.js
@@ -2,18 +2,13 @@ const jwt = require("jsonwebtoken");
 
 export function getJWTData() {
   const token = localStorage.getItem("jwt");
-
-  console.log("token is ", token);
-
   if (!token) return false;
 
   // get the decoded payload and header
   const decoded = jwt.decode(token, { complete: true });
-
   return decoded.payload;
 }
 
 export function generateJWTToken(payload) {
-  console.log("inside generate jwt", payload);
   return jwt.sign(payload, "something");
 }
