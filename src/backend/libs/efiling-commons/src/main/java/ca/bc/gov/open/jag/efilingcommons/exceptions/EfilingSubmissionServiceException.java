301e6a82949ae0e85474fd084eb7f96c9c7e777d
TayGov <Taylor.Clausen@gov.bc.ca>
2020-07-30 11:50:34 -0700

    Remove unused method

--- src/backend/libs/efiling-commons/src/main/java/ca/bc/gov/open/jag/efilingcommons/exceptions/EfilingSubmissionServiceException.java
+++ src/backend/libs/efiling-commons/src/main/java/ca/bc/gov/open/jag/efilingcommons/exceptions/EfilingSubmissionServiceException.java
@@ -1,11 +1,6 @@
 package ca.bc.gov.open.jag.efilingcommons.exceptions;
 
 public class EfilingSubmissionServiceException extends RuntimeException {
-
-    public EfilingSubmissionServiceException(String message) {
-        super(message);
-    }
-
     public EfilingSubmissionServiceException(String message, Throwable cause) {
         super(message, cause);
     }
