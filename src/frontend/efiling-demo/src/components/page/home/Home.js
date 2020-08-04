5e55bbde9ce5ec0c4607e4c7b9709e7e98b5fbbc
Shreyas <shreyas.devalapurkar@gov.bc.ca>
2020-08-04 12:15:27 -0700

    fix linting issue

--- src/frontend/efiling-demo/src/components/page/home/Home.js
+++ src/frontend/efiling-demo/src/components/page/home/Home.js
@@ -1,4 +1,4 @@
-/* eslint-disable react/jsx-curly-newline */
+/* eslint-disable react/jsx-curly-newline, camelcase */
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { v4 as uuidv4 } from "uuid";
