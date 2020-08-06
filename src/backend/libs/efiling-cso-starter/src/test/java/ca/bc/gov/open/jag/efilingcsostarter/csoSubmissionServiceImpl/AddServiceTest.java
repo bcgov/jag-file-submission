f565c3c5d735a38ae6a3c37e29d831e1997ab504
TayGov <Taylor.Clausen@gov.bc.ca>
2020-08-06 11:55:42 -0700

    Remove duplication

--- src/backend/libs/efiling-cso-starter/src/test/java/ca/bc/gov/open/jag/efilingcsostarter/csoSubmissionServiceImpl/AddServiceTest.java
+++ src/backend/libs/efiling-cso-starter/src/test/java/ca/bc/gov/open/jag/efilingcsostarter/csoSubmissionServiceImpl/AddServiceTest.java
@@ -50,7 +50,7 @@ public class AddServiceTest {
     @DisplayName("OK: addService called with any non-empty service")
     @Test
     public void testWithPopulatedSubmissionId() throws NestedEjbException_Exception, DatatypeConfigurationException {
-        Mockito.when(serviceFacadeBean.addService(any())).thenReturn(createService());
+        Mockito.when(serviceFacadeBean.addService(any())).thenReturn(TestHelpers.createService());
         EfilingService actual = sut.addService(TestHelpers.createBaseEfilingService());
         Assertions.assertEquals(BigDecimal.TEN, actual.getAccountId());
         Assertions.assertEquals(BigDecimal.TEN, actual.getClientId());
@@ -74,22 +74,4 @@ public class AddServiceTest {
         Assertions.assertThrows(EfilingSubmissionServiceException.class, () -> sut.addService(TestHelpers.createBaseEfilingService()));
     }
 
-    private Service createService() throws DatatypeConfigurationException {
-        Service service = new Service();
-        service.setAccountId(BigDecimal.TEN);
-        service.setClientId(BigDecimal.TEN);
-        service.setClientReferenceTxt(TestHelpers.CLIENT_REFERENCE_TXT);
-        service.setCourtFileNo(TestHelpers.COURT_FILE_NUMBER);
-        service.setDocumentsProcessed(TestHelpers.DOCUMENTS_PROCESSED);
-        service.setEntDtm(TestHelpers.getXmlDate(TestHelpers.DATE));
-        service.setEntUserId("10");
-        service.setServiceId(BigDecimal.TEN);
-        service.setServiceReceivedDtm(TestHelpers.getXmlDate(TestHelpers.DATE));
-        service.setServiceReceivedDtmText(TestHelpers.SERVICE_RECEIVED_DTM_TEXT);
-        service.setServiceSessionId(BigDecimal.TEN);
-        service.setServiceSubtypeCd(TestHelpers.SERVICE_SUBTYPE_CD);
-        service.setServiceTypeCd(TestHelpers.SERVICE_TYPE_CD);
-        service.setServiceTypeDesc(TestHelpers.SERVICE_TYPE_DESC);
-        return service;
-    }
 }
