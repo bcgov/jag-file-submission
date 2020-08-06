59d5913006715ea6705b7a54be6282f82c2f59f9
TayGov <Taylor.Clausen@gov.bc.ca>
2020-08-06 14:30:51 -0700

    Adding mapping

--- src/backend/efiling-api/src/main/java/ca/bc/gov/open/jag/efilingapi/submission/mappers/EfilingFilingPackageMapper.java
+++ src/backend/efiling-api/src/main/java/ca/bc/gov/open/jag/efilingapi/submission/mappers/EfilingFilingPackageMapper.java
@@ -1,11 +1,16 @@
 package ca.bc.gov.open.jag.efilingapi.submission.mappers;
 
 
+import ca.bc.gov.open.jag.efilingapi.api.model.Document;
 import ca.bc.gov.open.jag.efilingapi.submission.models.Submission;
+import ca.bc.gov.open.jag.efilingcommons.model.EfilingDocument;
 import ca.bc.gov.open.jag.efilingcommons.model.EfilingFilingPackage;
 
+import ca.bc.gov.open.jag.efilingcommons.model.EfilingPackageAuthority;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper
 public interface EfilingFilingPackageMapper {
@@ -17,4 +22,15 @@ public interface EfilingFilingPackageMapper {
     @Mapping(target = "ldcxCourtClassCd", source = "filingPackage.court.courtClass")
     @Mapping(target = "ldcxCourtLevelCd", source = "filingPackage.court.level")
     EfilingFilingPackage toEfilingFilingPackage(Submission submission);
+
+    @Mapping(target = "amendsAnotherDocumentYn", source = "isAmendment")
+    @Mapping(target = "clientFileNameTxt", source = "name")
+    @Mapping(target = "documentDescriptionTxt", source = "description")
+    @Mapping(target = "documentTypeCd", source = "type")
+    EfilingDocument toEfilingDocument(Document documentList);
+
+    @Mapping(target = "clientId", source = "clientId")
+    @Mapping(target = "accountId", source = "accountId")
+    @Mapping(target = "entUserId", source = "clientId")
+    EfilingPackageAuthority toPackageAuthority(Submission submission);
 }
