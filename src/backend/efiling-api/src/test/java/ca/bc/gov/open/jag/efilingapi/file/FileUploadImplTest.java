d87689c5d4e74222bf216639b8f96b12231da706
TayGov <Taylor.Clausen@gov.bc.ca>
2020-08-07 11:12:59 -0700

    MOAR test

--- src/backend/efiling-api/src/test/java/ca/bc/gov/open/jag/efilingapi/file/FileUploadImplTest.java
+++ src/backend/efiling-api/src/test/java/ca/bc/gov/open/jag/efilingapi/file/FileUploadImplTest.java
@@ -1,24 +1,41 @@
 package ca.bc.gov.open.jag.efilingapi.file;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import ca.bc.gov.open.jag.efilingcommons.exceptions.EfilingFileException;
+import org.junit.jupiter.api.*;
+import org.mockito.MockitoAnnotations;
+
+import java.io.IOException;
+import java.nio.file.Files;
+
+import static org.hamcrest.Matchers.any;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @DisplayName("FileUploadImplTest test suite")
 public class FileUploadImplTest {
+    private static final String LOCATION = "LOCATION";
     FileUploadServiceImpl sut;
+
+    @BeforeAll
+    public void setUp() throws IOException {
+        MockitoAnnotations.initMocks(this);
+        FileUploadProperties fileUploadProperties = new FileUploadProperties();
+        fileUploadProperties.setLocation(LOCATION);
+
+        sut = new FileUploadServiceImpl(fileUploadProperties);
+    }
 
     @Test
     @DisplayName("OK: files are uploaded")
     public void withByteArrayUploadFile() {
 
+
+       // PowerMockito.doNothing().when(Files.copy()).copy();
     }
 
     @Test
     @DisplayName("Exception files failed")
     public void updloadThrowsException() {
-
+        Assertions.assertThrows(EfilingFileException.class, () -> sut.upload(null, null));
     }
 
 }
