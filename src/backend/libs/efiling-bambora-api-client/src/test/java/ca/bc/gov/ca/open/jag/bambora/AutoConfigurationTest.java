c8ff1e3869ba5d36a495235ca0e7c539add44ede
TayGov <Taylor.Clausen@gov.bc.ca>
2020-08-11 11:18:17 -0700

    Add test

--- src/backend/libs/efiling-bambora-api-client/src/test/java/ca/bc/gov/ca/open/jag/bambora/AutoConfigurationTest.java
+++ src/backend/libs/efiling-bambora-api-client/src/test/java/ca/bc/gov/ca/open/jag/bambora/AutoConfigurationTest.java
@@ -4,11 +4,14 @@ import ca.bc.gov.open.jag.bambora.AutoConfiguration;
 
 import ca.bc.gov.open.jag.efilingbamboraapiclient.api.PaymentsApi;
 import ca.bc.gov.open.jag.efilingbamboraapiclient.api.handler.ApiClient;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DisplayName("AutoConfiguration")
 public class AutoConfigurationTest {
     ApplicationContextRunner context = new ApplicationContextRunner()
             .withUserConfiguration(AutoConfiguration.class);
