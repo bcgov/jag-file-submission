79b762d4cd31b815a610e2012b08ab0d178a0923
alex <alexandre.joyeux@nttdata.com>
2020-08-12 14:41:07 -0700

    rafactoring payment

--- src/backend/efiling-api/src/main/java/ca/bc/gov/open/jag/efilingapi/payment/BamboraPaymentAdapter.java
+++ src/backend/efiling-api/src/main/java/ca/bc/gov/open/jag/efilingapi/payment/BamboraPaymentAdapter.java
@@ -2,7 +2,7 @@ package ca.bc.gov.open.jag.efilingapi.payment;
 
 import ca.bc.gov.open.jag.efilingbamboraapiclient.api.PaymentsApi;
 import ca.bc.gov.open.jag.efilingbamboraapiclient.api.handler.ApiException;
-import ca.bc.gov.open.jag.efilingbamboraapiclient.api.model.CardOnFile;
+import ca.bc.gov.open.jag.efilingbamboraapiclient.api.model.Custom;
 import ca.bc.gov.open.jag.efilingbamboraapiclient.api.model.PaymentRequest;
 import ca.bc.gov.open.jag.efilingbamboraapiclient.api.model.PaymentResponse;
 import ca.bc.gov.open.jag.efilingbamboraapiclient.api.model.ProfilePurchase;
@@ -19,6 +19,10 @@ import java.math.BigDecimal;
 @Service
 public class BamboraPaymentAdapter {
 
+    public static final String COURT_SERVICES = "COURT SERVICES";
+    public static final int CARD_ID = 1;
+    public static final String DECLINED = "DEC";
+    public static final String APPROVED = "APP";
     Logger logger = LoggerFactory.getLogger(BamboraPaymentAdapter.class);
 
     private final PaymentsApi paymentsApi;
@@ -29,40 +33,48 @@ public class BamboraPaymentAdapter {
 
     public EfilingTransaction makePayment(EfilingPayment efilingPayment) {
 
-        PaymentRequest paymentRequest = new PaymentRequest();
-        paymentRequest.amount(efilingPayment.getPaymentAmount().doubleValue());
-        paymentRequest.setOrderNumber(efilingPayment.getInvoiceNumber());
-
-        paymentRequest.setPaymentMethod(PaymentRequest.PaymentMethodEnum.PAYMENT_PROFILE);
-
-        ProfilePurchase profilePurchase = new ProfilePurchase();
-        profilePurchase.setCustomerCode(efilingPayment.getInternalClientNumber());
-        profilePurchase.setCardId(1);
-        paymentRequest.setPaymentProfile(profilePurchase);
-
-
         EfilingTransaction result = new EfilingTransaction();
 
         try {
-            PaymentResponse response = paymentsApi.makePayment(paymentRequest);
+
+            PaymentResponse response = paymentsApi.makePayment(buildPaymentRequest(efilingPayment));
             result.setEcommerceTransactionId(BigDecimal.valueOf(response.getMessageId()));
             result.setEntDtm(DateUtils.getCurrentXmlDate());
             result.setTransactonDtm(DateUtils.getCurrentXmlDate());
             result.setTransactionAmt(BigDecimal.valueOf(response.getAmount()));
             result.setResponseCd(response.getAuthCode());
             result.setInvoiceNo(response.getOrderNumber());
-            //Reference: https://dev.na.bambora.com/docs/references/payment_APIs/payment_profile_response_codes/
-            //TODO: what are the appropriate codes
-            if (response.getApproved() == 1) {
-                result.setApprovalCd("Approved");
-            } else {
-                result.setApprovalCd("Failed");
-            }
+            result.setApprovalCd(response.getApproved() == CARD_ID ? APPROVED : DECLINED);
+
             return result;
+
         } catch (ApiException e) {
             logger.error("Bambora payment exception", e);
             throw new EfilingSubmissionServiceException("Bambora payment exception", e.getCause());
         }
+
+    }
+
+    private PaymentRequest buildPaymentRequest(EfilingPayment efilingPayment) {
+
+        PaymentRequest paymentRequest = new PaymentRequest();
+
+        paymentRequest.setPaymentMethod(PaymentRequest.PaymentMethodEnum.PAYMENT_PROFILE);
+        paymentRequest.amount(efilingPayment.getPaymentAmount().doubleValue());
+        paymentRequest.setOrderNumber(efilingPayment.getInvoiceNumber());
+
+        Custom customReference = new Custom();
+        customReference.setRef1(efilingPayment.getInternalClientNumber());
+        customReference.setRef2(COURT_SERVICES);
+        customReference.setRef3(efilingPayment.getServiceId().toString());
+        paymentRequest.setCustom(customReference);
+
+        ProfilePurchase profilePurchase = new ProfilePurchase();
+        profilePurchase.setCustomerCode(efilingPayment.getInternalClientNumber());
+        profilePurchase.setCardId(CARD_ID);
+        profilePurchase.setComplete(true);
+        paymentRequest.setPaymentProfile(profilePurchase);
+        return paymentRequest;
 
     }
 }
