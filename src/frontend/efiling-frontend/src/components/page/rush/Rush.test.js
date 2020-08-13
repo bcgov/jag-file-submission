650ac35e2657b5f205dd385c0923814d7dc84249
Shreyas <shreyas.devalapurkar@gov.bc.ca>
2020-08-13 14:21:48 -0700

    add test case

--- src/frontend/efiling-frontend/src/components/page/rush/Rush.test.js
+++ src/frontend/efiling-frontend/src/components/page/rush/Rush.test.js
@@ -1,9 +1,10 @@
 // TODO: remove ignoring Rush.js from coverage report
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, getByText } from "@testing-library/react";
 import { getDocumentsData } from "../../../modules/test-data/documentTestData";
 import { getTestData } from "../../../modules/test-data/confirmationPopupTestData";
 import { getCourtData } from "../../../modules/test-data/courtTestData";
+import { generateJWTToken } from "../../../modules/helpers/authentication-helper/authenticationHelper";
 
 import Rush from "./Rush";
 
@@ -22,8 +23,21 @@ describe("Rush Component", () => {
     submissionFee,
   };
 
+  const token = generateJWTToken({ preferred_username: "username@bceid" });
+  localStorage.setItem("jwt", token);
+
   test("Matches the snapshot", () => {
     const { asFragment } = render(<Rush payment={payment} />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  test("Clicking on cancel request takes user back to payment page", () => {
+    const { container, asFragment } = render(<Rush payment={payment} />);
+
+    const button = getByText(container, "Cancel Request");
+
+    fireEvent.click(button);
 
     expect(asFragment()).toMatchSnapshot();
   });
