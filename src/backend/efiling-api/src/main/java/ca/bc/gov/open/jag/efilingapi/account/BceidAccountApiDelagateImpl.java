54b7e98f23b7a171b94901ca820032980475357e
alex <alexandre.joyeux@nttdata.com>
2020-08-14 15:06:36 -0700

    add as a service

--- src/backend/efiling-api/src/main/java/ca/bc/gov/open/jag/efilingapi/account/BceidAccountApiDelagateImpl.java
+++ src/backend/efiling-api/src/main/java/ca/bc/gov/open/jag/efilingapi/account/BceidAccountApiDelagateImpl.java
@@ -3,10 +3,12 @@ package ca.bc.gov.open.jag.efilingapi.account;
 import ca.bc.gov.open.jag.efilingapi.api.BceidAccountApiDelegate;
 import ca.bc.gov.open.jag.efilingapi.api.model.BceidAccount;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.security.RolesAllowed;
 import java.util.UUID;
 
+@Service
 public class BceidAccountApiDelagateImpl implements BceidAccountApiDelegate {
 
     @Override
