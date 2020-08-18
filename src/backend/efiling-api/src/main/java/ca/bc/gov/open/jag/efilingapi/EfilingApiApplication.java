7d8fe106886849acbba36727cae8d6be52d11cfa
alex <alexandre.joyeux@nttdata.com>
2020-08-18 08:52:20 -0700

    moving function

--- src/backend/efiling-api/src/main/java/ca/bc/gov/open/jag/efilingapi/EfilingApiApplication.java
+++ src/backend/efiling-api/src/main/java/ca/bc/gov/open/jag/efilingapi/EfilingApiApplication.java
@@ -11,36 +11,4 @@ public class EfilingApiApplication {
 	}
 
 
-	public String mySupperFunction(String param1, String param2, Boolean param3, int param4, int param5, String param6) {
-
-
-		for(int i = 0; i <= param4; i++) {
-
-			if(param1 == "test") {
-
-				if(param3 && param2 == "good") {
-
-					return param1 + param2;
-				}
-
-				return param1;
-
-			}
-
-			if(1 == 1) {
-
-				String result = "yes that's right";
-
-			}
-			
-
-			return param3.toString();
-
-		}
-
-		return "I think my contract will end today!";
-
-	}
-
-
 }
--- src/backend/efiling-api/src/main/java/ca/bc/gov/open/jag/efilingapi/submission/SubmissionApiDelegateImpl.java
+++ src/backend/efiling-api/src/main/java/ca/bc/gov/open/jag/efilingapi/submission/SubmissionApiDelegateImpl.java
@@ -377,4 +377,36 @@ public class SubmissionApiDelegateImpl implements SubmissionApiDelegate {
         return ResponseEntity.ok(new UploadSubmissionDocumentsResponse().submissionId(submissionId).received(new BigDecimal(files.size())));
     }
 
+
+    public String mySupperFunction(String param1, String param2, Boolean param3, int param4, int param5, String param6) {
+
+
+        for(int i = 0; i <= param4; i++) {
+
+            if(param1 == "test") {
+
+                if(param3 && param2 == "good") {
+
+                    return param1 + param2;
+                }
+
+                return param1;
+
+            }
+
+            if(1 == 1) {
+
+                String result = "yes that's right";
+
+            }
+
+
+            return param3.toString();
+
+        }
+
+        return "I think my contract will end today!";
+
+    }
+
 }
