c84c8ce686d44b2076741db774d4870d15b05f77
alex <alexandre.joyeux@nttdata.com>
2020-08-20 10:37:14 -0700

    adding get the cso-application-code from token

--- src/backend/efiling-api/src/main/java/ca/bc/gov/open/jag/efilingapi/Keys.java
+++ src/backend/efiling-api/src/main/java/ca/bc/gov/open/jag/efilingapi/Keys.java
@@ -2,6 +2,8 @@ package ca.bc.gov.open.jag.efilingapi;
 
 public class Keys {
 
+
+
     protected Keys() {}
 
     public static final String EFILING_APP = "efiling";
@@ -15,5 +17,7 @@ public class Keys {
     public static final String MDC_EFILING_CLIENT_ID = EFILING_APP + ".clientId";
 
     public static final String UNIVERSAL_ID_CLAIM_KEY = "universal-id";
+
+    public static final String CSO_APPLICATION_CODE = "cso-application-code";
 
 }
