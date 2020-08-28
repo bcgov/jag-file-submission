913656a6d3581f9b5417f4140b1f9a0ecaac102b
karthikmsqa <karthikmsqa@gmail.com>
2020-08-27 18:45:17 -0700

    fix more cc suggestions

--- tests/src/main/java/ca/bc/gov/open/jagefilingapi/qa/backendutils/TestUtil.java
+++ tests/src/main/java/ca/bc/gov/open/jagefilingapi/qa/backendutils/TestUtil.java
@@ -11,11 +11,18 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
 
 
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
 
 import static io.restassured.RestAssured.baseURI;
 
@@ -81,5 +88,22 @@ public class TestUtil {
         String resp = response.asString();
         JsonPath jsonPath = new JsonPath(resp);
         return jsonPath.get(key);
+    }
+
+    public static List<String> getSubmissionAndTransId(String respUrl, String submissionId, String transactionId) throws URISyntaxException {
+
+        List<NameValuePair> params = URLEncodedUtils.parse(new URI(respUrl), StandardCharsets.UTF_8);
+
+        String respSubId = null;
+        String respTransId = null;
+        
+        for (NameValuePair param : params) {
+            if (param.getName().equals(submissionId)) {
+                respSubId = param.getValue();
+            } else if (param.getName().equals(transactionId)) {
+                respTransId = param.getValue();
+            }
+        }
+        return Arrays.asList(respSubId, respTransId);
     }
 }
