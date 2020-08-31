3c6d35bf6e5e0be345d3a378f9784ce5adaad5e3
alex <alexandre.joyeux@nttdata.com>
2020-08-31 12:33:43 -0700

    fix demo

--- src/backend/libs/efiling-demo-starter/src/main/java/ca/bc/gov/open/jag/efiling/demo/EfilingSubmissionServiceDemoImpl.java
+++ src/backend/libs/efiling-demo-starter/src/main/java/ca/bc/gov/open/jag/efiling/demo/EfilingSubmissionServiceDemoImpl.java
@@ -1,5 +1,6 @@
 package ca.bc.gov.open.jag.efiling.demo;
 
+import ca.bc.gov.open.jag.efilingcommons.model.AccountDetails;
 import ca.bc.gov.open.jag.efilingcommons.model.EfilingFilingPackage;
 import ca.bc.gov.open.jag.efilingcommons.model.EfilingService;
 import ca.bc.gov.open.jag.efilingcommons.service.EfilingPaymentService;
@@ -10,7 +11,7 @@ import java.math.BigDecimal;
 public class EfilingSubmissionServiceDemoImpl implements EfilingSubmissionService {
 
     @Override
-    public BigDecimal submitFilingPackage(EfilingService service, EfilingFilingPackage filingPackage, boolean isRushedProcessing,  EfilingPaymentService efilingPaymentService) {
+    public BigDecimal submitFilingPackage(AccountDetails accountDetails, EfilingService service, EfilingFilingPackage filingPackage, boolean isRushedProcessing, EfilingPaymentService efilingPaymentService) {
         return BigDecimal.ONE;
     }
 
--- src/backend/libs/efiling-demo-starter/src/test/java/ca/bc/gov/open/jag/efiling/demo/filingSubmissionServiceDemoImplTest.java
+++ src/backend/libs/efiling-demo-starter/src/test/java/ca/bc/gov/open/jag/efiling/demo/filingSubmissionServiceDemoImplTest.java
@@ -16,7 +16,7 @@ public class filingSubmissionServiceDemoImplTest {
 
         EfilingSubmissionServiceDemoImpl service = new EfilingSubmissionServiceDemoImpl();
 
-        BigDecimal actual = service.submitFilingPackage(null, null, false, null);
+        BigDecimal actual = service.submitFilingPackage(null,null, null, false, null);
 
         Assertions.assertEquals(BigDecimal.ONE, actual);
 
