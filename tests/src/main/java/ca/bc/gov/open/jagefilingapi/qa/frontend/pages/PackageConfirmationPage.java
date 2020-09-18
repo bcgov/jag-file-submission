4baf232ae18513fa2428f4a5a49bd0108cd88045
karthikmsqa <karthikmsqa@gmail.com>
2020-09-18 07:09:15 -0700

    trying screenshot artifact

--- tests/src/main/java/ca/bc/gov/open/jagefilingapi/qa/frontend/pages/PackageConfirmationPage.java
+++ tests/src/main/java/ca/bc/gov/open/jagefilingapi/qa/frontend/pages/PackageConfirmationPage.java
@@ -1,5 +1,6 @@
 package ca.bc.gov.open.jagefilingapi.qa.frontend.pages;
 
+import ca.bc.gov.open.jagefilingapi.qa.frontendutils.DriverClass;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,7 +12,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PackageConfirmationPage {
+public class PackageConfirmationPage extends DriverClass {
 
     private final WebDriver driver;
 
@@ -42,9 +43,18 @@ public class PackageConfirmationPage {
     }
 
     public void clickUploadLink() {
-        WebDriverWait wait = new WebDriverWait(driver, 120);
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@data-test-id='upload-link']")));
-        uploadLink.click();
+        try {
+            for (int i = 0; i < 3; i++) {
+                WebDriverWait wait = new WebDriverWait(driver, 120);
+                wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@data-test-id='upload-link']")));
+                uploadLink.click();
+                if (!uploadLink.isDisplayed()) {
+                    break;
+                }
+            }
+        } catch (org.openqa.selenium.TimeoutException tx) {
+            log.info("Upload link is not displayed in the page");
+        }
     }
 
     public List<String> getUploadedFilesList() {
