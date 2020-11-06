11493e555d7fe51de2c2d2a8f1d6ee871bcd4dda
taylorclausen <taylor.clausen@nttdata.com>
2020-11-06 09:55:25 -0800

    Adding mock data

--- src/backend/efiling-api/src/test/java/ca/bc/gov/open/jag/efilingapi/courts/GetCourtLocationsTest.java
+++ src/backend/efiling-api/src/test/java/ca/bc/gov/open/jag/efilingapi/courts/GetCourtLocationsTest.java
@@ -28,18 +28,30 @@ public class GetCourtLocationsTest {
         ResponseEntity<CourtLocations> actual = sut.getCourtLocations(COURTLEVEL);
 
         Assertions.assertEquals(HttpStatus.OK, actual.getStatusCode());
-        Assertions.assertEquals(1, actual.getBody().getCourts().size());
-        Assertions.assertEquals(BigDecimal.ONE, actual.getBody().getCourts().get(0).getId());
-        Assertions.assertEquals("Test",actual.getBody().getCourts().get(0).getName());
-        Assertions.assertEquals("Test",actual.getBody().getCourts().get(0).getCode());
+        Assertions.assertEquals(2, actual.getBody().getCourts().size());
+        Assertions.assertEquals(BigDecimal.valueOf(1031), actual.getBody().getCourts().get(0).getId());
+        Assertions.assertEquals("Campbell River",actual.getBody().getCourts().get(0).getName());
+        Assertions.assertEquals("MockCode",actual.getBody().getCourts().get(0).getCode());
         Assertions.assertEquals(true,actual.getBody().getCourts().get(0).getIsSupremeCourt());
-        Assertions.assertEquals("Test",actual.getBody().getCourts().get(0).getAddress().getAddressLine1());
-        Assertions.assertEquals("Test",actual.getBody().getCourts().get(0).getAddress().getAddressLine2());
-        Assertions.assertEquals("Test",actual.getBody().getCourts().get(0).getAddress().getAddressLine3());
-        Assertions.assertEquals("Test",actual.getBody().getCourts().get(0).getAddress().getPostalCode());
-        Assertions.assertEquals("Test",actual.getBody().getCourts().get(0).getAddress().getCityName());
-        Assertions.assertEquals("Test",actual.getBody().getCourts().get(0).getAddress().getProvinceName());
-        Assertions.assertEquals("Test",actual.getBody().getCourts().get(0).getAddress().getCountryName());
+        Assertions.assertEquals("500 - 13th Avenue",actual.getBody().getCourts().get(0).getAddress().getAddressLine1());
+        Assertions.assertEquals("",actual.getBody().getCourts().get(0).getAddress().getAddressLine2());
+        Assertions.assertEquals("",actual.getBody().getCourts().get(0).getAddress().getAddressLine3());
+        Assertions.assertEquals("V9W 6P1",actual.getBody().getCourts().get(0).getAddress().getPostalCode());
+        Assertions.assertEquals("Campbell River",actual.getBody().getCourts().get(0).getAddress().getCityName());
+        Assertions.assertEquals("British Columbia",actual.getBody().getCourts().get(0).getAddress().getProvinceName());
+        Assertions.assertEquals("Canada",actual.getBody().getCourts().get(0).getAddress().getCountryName());
+
+        Assertions.assertEquals(BigDecimal.valueOf(3521), actual.getBody().getCourts().get(1).getId());
+        Assertions.assertEquals("Chilliwack",actual.getBody().getCourts().get(1).getName());
+        Assertions.assertEquals("MockCode",actual.getBody().getCourts().get(1).getCode());
+        Assertions.assertEquals(true,actual.getBody().getCourts().get(1).getIsSupremeCourt());
+        Assertions.assertEquals("46085 Yale Road",actual.getBody().getCourts().get(1).getAddress().getAddressLine1());
+        Assertions.assertEquals("",actual.getBody().getCourts().get(1).getAddress().getAddressLine2());
+        Assertions.assertEquals("",actual.getBody().getCourts().get(1).getAddress().getAddressLine3());
+        Assertions.assertEquals("V2P 2L8",actual.getBody().getCourts().get(1).getAddress().getPostalCode());
+        Assertions.assertEquals("Chilliwack",actual.getBody().getCourts().get(1).getAddress().getCityName());
+        Assertions.assertEquals("British Columbia",actual.getBody().getCourts().get(1).getAddress().getProvinceName());
+        Assertions.assertEquals("Canada",actual.getBody().getCourts().get(1).getAddress().getCountryName());
 
     }
 }
