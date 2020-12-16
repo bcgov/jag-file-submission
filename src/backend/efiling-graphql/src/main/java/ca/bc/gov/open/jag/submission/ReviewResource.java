5253d4cefb60bb3f654f01cc9a0eb50d09a6e4e0
taylorclausen <taylor.clausen@nttdata.com>
2020-12-16 07:58:00 -0800

    remove useless imports

--- src/backend/efiling-graphql/src/main/java/ca/bc/gov/open/jag/submission/ReviewResource.java
+++ src/backend/efiling-graphql/src/main/java/ca/bc/gov/open/jag/submission/ReviewResource.java
@@ -1,11 +1,9 @@
 package ca.bc.gov.open.jag.submission;
 
-import ca.bc.gov.open.jag.efilingcommons.service.EfilingSubmissionService;
 import org.eclipse.microprofile.graphql.Description;
 import org.eclipse.microprofile.graphql.GraphQLApi;
 import org.eclipse.microprofile.graphql.Query;
 
-import javax.inject.Inject;
 
 @GraphQLApi
 public class ReviewResource {
