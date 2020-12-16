b9980f0762147105b22b357fd7746abe54d50aff
taylorclausen <taylor.clausen@nttdata.com>
2020-12-16 14:42:46 -0800

    Remove exception

--- src/backend/libs/efiling-commons/src/main/java/ca/bc/gov/open/jag/efilingcommons/service/EfilingStatusService.java
+++ src/backend/libs/efiling-commons/src/main/java/ca/bc/gov/open/jag/efilingcommons/service/EfilingStatusService.java
@@ -2,11 +2,10 @@ package ca.bc.gov.open.jag.efilingcommons.service;
 
 import ca.bc.gov.open.jag.efilingcommons.model.FilePackage;
 
-import javax.xml.datatype.DatatypeConfigurationException;
 import java.math.BigDecimal;
 
 public interface EfilingStatusService {
 
-    FilePackage findStatusByPackage(BigDecimal clientId, BigDecimal packageNo) throws DatatypeConfigurationException;
+    FilePackage findStatusByPackage(BigDecimal clientId, BigDecimal packageNo);
 
 }
