d9904a412ff9fedd316d1b9c134f365f309dc597
taylorclausen <taylor.clausen@nttdata.com>
2020-12-16 07:35:28 -0800

    Adding service

--- src/backend/efiling-graphql/src/main/java/ca/bc/gov/open/jag/submission/ReviewResource.java
+++ src/backend/efiling-graphql/src/main/java/ca/bc/gov/open/jag/submission/ReviewResource.java
@@ -1,11 +1,17 @@
 package ca.bc.gov.open.jag.submission;
 
+import ca.bc.gov.open.jag.efilingcommons.service.EfilingSubmissionService;
 import org.eclipse.microprofile.graphql.Description;
 import org.eclipse.microprofile.graphql.GraphQLApi;
 import org.eclipse.microprofile.graphql.Query;
 
+import javax.inject.Inject;
+
 @GraphQLApi
 public class ReviewResource {
+
+    @Inject
+    EfilingSubmissionService efilingSubmissionService;
 
     @Query("submissionReview")
     @Description("Query a submission")
