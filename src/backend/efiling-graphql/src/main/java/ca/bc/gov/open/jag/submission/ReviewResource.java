e1924984f85fda8b79bfbd1ec43481cfa96d86a6
taylorclausen <taylor.clausen@nttdata.com>
2020-12-18 07:47:03 -0800

    Adding quarkus object

--- src/backend/efiling-graphql/src/main/java/ca/bc/gov/open/jag/submission/ReviewResource.java
+++ src/backend/efiling-graphql/src/main/java/ca/bc/gov/open/jag/submission/ReviewResource.java
@@ -1,7 +1,9 @@
 package ca.bc.gov.open.jag.submission;
 
+import ca.bc.gov.ag.csows.filing.status.File;
 import ca.bc.gov.open.jag.efilingcommons.model.FilePackage;
 import ca.bc.gov.open.jag.efilingcommons.service.EfilingStatusService;
+import ca.bc.gov.open.jag.submission.model.FilePackageOutput;
 import org.eclipse.microprofile.graphql.Description;
 import org.eclipse.microprofile.graphql.GraphQLApi;
 import org.eclipse.microprofile.graphql.Name;
@@ -10,6 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 
 @GraphQLApi
@@ -26,8 +29,19 @@ public class ReviewResource {
 
     @Query
     @Description("Query a package")
-    public FilePackage getSubmission(@Name("clientId") BigDecimal clientId, @Name("packageNo") BigDecimal packageNo) {
+    public FilePackageOutput getSubmission(@Name("clientId") BigDecimal clientId, @Name("packageNo") BigDecimal packageNo) {
         logger.info("GraphQl Request received");
-        return efilingStatusService.findStatusByPackage(clientId, packageNo).get();
+
+        Optional<FilePackage> filePackage = efilingStatusService.findStatusByPackage(clientId, packageNo);
+
+        FilePackageOutput filePackageOutput = new FilePackageOutput();
+
+        if (filePackage.isPresent()) {
+            filePackageOutput.setCourtClass(filePackage.get().getCourtClassCd());
+            filePackageOutput.setFileNumber(filePackage.get().getClientFileNo());
+        }
+
+
+        return filePackageOutput;
     }
 }
