1eb19ca36b9836d4572532645e33232581d69690
taylorclausen <taylor.clausen@nttdata.com>
2020-12-21 07:48:45 -0800

    Simple implementation

---
+++ src/backend/efiling-api/src/main/java/ca/bc/gov/open/jag/efilingapi/filePackage/FilepackageApiDelegateImpl.java
@@ -0,0 +1,14 @@
+package ca.bc.gov.open.jag.efilingapi.filePackage;
+
+import ca.bc.gov.open.jag.efilingapi.api.FilepackageApiDelegate;
+import ca.bc.gov.open.jag.efilingapi.api.model.FilingPackage;
+import org.springframework.http.ResponseEntity;
+
+import java.math.BigDecimal;
+
+public class FilepackageApiDelegateImpl implements FilepackageApiDelegate {
+    @Override
+    public ResponseEntity<FilingPackage> getFilePackage(BigDecimal clientId, BigDecimal packageNumber) {
+        return ResponseEntity.ok(new FilingPackage());
+    }
+}
