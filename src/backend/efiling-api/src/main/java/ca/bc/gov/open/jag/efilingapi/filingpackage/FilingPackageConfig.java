b1596742651f468a28ce3b47b2eb71976d1921f1
taylorclausen <taylor.clausen@nttdata.com>
2020-12-22 11:25:31 -0800

    Missing annotations

--- src/backend/efiling-api/src/main/java/ca/bc/gov/open/jag/efilingapi/filingpackage/FilingPackageConfig.java
+++ src/backend/efiling-api/src/main/java/ca/bc/gov/open/jag/efilingapi/filingpackage/FilingPackageConfig.java
@@ -7,7 +7,9 @@ import ca.bc.gov.open.jag.efilingapi.filingpackage.service.FilingPackageService;
 import ca.bc.gov.open.jag.efilingapi.filingpackage.service.FilingPackageServiceImpl;
 import ca.bc.gov.open.jag.efilingcommons.submission.EfilingStatusService;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class FilingPackageConfig {
     @Bean
     public FilingPackageService filePackageService(EfilingStatusService efilingStatusService, AccountService accountService) {
