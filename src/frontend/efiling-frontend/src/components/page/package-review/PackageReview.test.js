d0c4ca573f33a7084fa00dd78b15e81cb52092d3
Brendan Beach <Brendan.Beach@gov.bc.ca>
2021-01-05 13:35:40 -0800

    Added test for Api call to maintain coverage

--- src/frontend/efiling-frontend/src/components/page/package-review/PackageReview.test.js
+++ src/frontend/efiling-frontend/src/components/page/package-review/PackageReview.test.js
@@ -1,8 +1,10 @@
 import React from "react";
+import axios from "axios";
 import { createMemoryHistory } from "history";
 import { render, fireEvent, getByText } from "@testing-library/react";
 
 import PackageReview from "./PackageReview";
+import MockAdapter from "axios-mock-adapter";
 
 describe("PackageReview Component", () => {
   const header = {
@@ -16,7 +18,13 @@ describe("PackageReview Component", () => {
     packageId,
   };
 
-  window.open = jest.fn();
+  let mock;
+  beforeEach(() => {
+    mock = new MockAdapter(axios);
+    window.open = jest.fn();
+  });
+
+  const apiRequest = `/filingpackage/${packageId}`;
 
   test("Matches the snapshot", () => {
     const { asFragment } = render(<PackageReview page={page} />);
@@ -30,5 +38,16 @@ describe("PackageReview Component", () => {
     fireEvent.click(getByText(container, "Cancel and Return to Parent App"));
 
     expect(window.open).toHaveBeenCalledWith("http://google.com", "_self");
+  });
+
+  test("Api is called successfully when page loads with valid packageId", async () => {
+    window.open = jest.fn();
+    mock.onGet(apiRequest).reply(200);
+
+    let spy = jest.spyOn(axios, "get");
+
+    render(<PackageReview page={page} />);
+
+    expect(spy).toHaveBeenCalled();
   });
 });
