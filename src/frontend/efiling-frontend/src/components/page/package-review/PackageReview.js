67fbaf62c987863cd30d94d23485f2fcfa4e25ce
Kevin Neufeld <Kevin.Neufeld@nttdata.com>
2021-01-13 12:46:21 -0800

    FLA-670 updated PackageReview to show submitted data

--- src/frontend/efiling-frontend/src/components/page/package-review/PackageReview.js
+++ src/frontend/efiling-frontend/src/components/page/package-review/PackageReview.js
@@ -1,30 +1,89 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import moment from 'moment';
 import PropTypes from "prop-types";
-import { Header, Footer, Button } from "shared-components";
+import { Header, Footer, Button, Table, Alert } from "shared-components";
+import { MdCancel } from "react-icons/md";
 import axios from "axios";
 import { propTypes } from "../../../types/propTypes";
 
 export default function PackageReview({ page: { header, packageId } }) {
+  const [error, setError] = useState(false);
+  const [packageNoDetails, setPackageNoDetails] = useState([
+    { name: 'Package Number:', value: '', isNameBold: false, isValueBold: true },
+    { name: 'Submitted By:' , value: '', isNameBold: false, isValueBold: true  },
+    { name: 'Submitted Date:' , value: '', isNameBold: false, isValueBold: true  }
+  ]);
+  const [courtFileDetails, setCourtFileDetails] = useState([
+    { name: 'Court File Number:', value: '', isNameBold: false, isValueBold: true  },
+    { name: 'Submitted To:' , value: '', isNameBold: false, isValueBold: true  },
+    { name: 'Filing Comments:' , value: '', isNameBold: false, isValueBold: true  }
+  ]);
+
   useEffect(() => {
     console.log(packageId);
     axios
       .get(`filingpackage/${packageId}`)
       .then((response) => {
-        console.log(response);
+        try {          
+          let packageNo = response.data.packageNumber || 'n/a';
+          let submittedBy = response.data.submittedBy || 'n/a';
+          let submittedDt = 'n/a';
+          if (response.data.submittedDate) {
+            submittedDt = moment(response.data.submittedDate).format('DD-MMM-YYYY HH:MM');
+          }
+          let fileNumber = response.data.court.fileNumber || 'n/a';
+          let submittedTo = response.data.court.location || 'n/a';
+          let filingComments = response.data.court.fileNumber || 'n/a';
+          setPackageNoDetails([
+            { name: 'Package Number:', value: packageNo + '', isNameBold: false, isValueBold: true },
+            { name: 'Submitted By:' , value: submittedBy, isNameBold: false, isValueBold: true  },
+            { name: 'Submitted Date:' , value: submittedDt, isNameBold: false, isValueBold: true  }
+          ]);
+          setCourtFileDetails([
+            { name: 'Court File Number:', value: fileNumber + '', isNameBold: false, isValueBold: true  },
+            { name: 'Submitted To:' , value: submittedTo + '', isNameBold: false, isValueBold: true  },
+            { name: 'Filing Comments:' , value: filingComments + '', isNameBold: false, isValueBold: true  }
+          ]);
+        }
+        catch(error) {
+          console.log(error);
+          setError(true);
+        }
       })
       .catch((error) => {
         console.log(error);
+        setError(true);
       });
-  });
+  }, []);
 
   return (
     <main>
       <Header header={header} />
       <div className="page">
-        <div className="content col-md-8">
-          <h1>Package Review!</h1>
+        <div className="content col-md-12">
+          <h1>View Submitted Package</h1>
+          {error && (
+            <div className="col-md-12">
+              <Alert
+                icon={<MdCancel size={24} />}
+                type="error"
+                styling="bcgov-error-background"
+                element="There was a problem with your request."
+              />
+            </div>
+          )}
           <br />
-          <p>{`We are dealing with package ID: ${packageId}`}</p>
+          <h2>Package Details</h2>
+          <br />
+          <div className="row">
+            <div className="col-sm-12 col-lg-6">
+              <Table elements={packageNoDetails} />
+            </div>
+            <div className="col-sm-12 col-lg-6">
+              <Table elements={courtFileDetails} />
+            </div>
+          </div>
+          <br />
           <br />
           <section className="buttons pt-2">
             <Button
