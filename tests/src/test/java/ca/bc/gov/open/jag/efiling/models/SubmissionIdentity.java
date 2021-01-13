e0fdfc8095d4b0920e83277b35d47f7679885a89
Karthikeyan <karthikmsqa@gmail.com>
2021-01-13 13:09:58 -0800

    trying implementing service

---
+++ tests/src/test/java/ca/bc/gov/open/jag/efiling/models/SubmissionIdentity.java
@@ -0,0 +1,15 @@
+package ca.bc.gov.open.jag.efiling.models;
+
+public class SubmissionIdentity {
+
+    private String submissionId;
+
+    public SubmissionIdentity(String submissionId) {
+        this.submissionId = submissionId;
+    }
+
+    public String getSubmissionId() {
+        return submissionId;
+    }
+
+}
