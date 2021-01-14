8d95be360408aa0a16732157378f88adab65140a
Karthikeyan <karthikmsqa@gmail.com>
2021-01-14 10:17:20 -0800

    submission helper and service implementation

--- tests/src/test/java/ca/bc/gov/open/jag/efiling/stepDefinitions/GenerateUrlSD.java
+++ tests/src/test/java/ca/bc/gov/open/jag/efiling/stepDefinitions/GenerateUrlSD.java
@@ -1,21 +1,18 @@
 package ca.bc.gov.open.jag.efiling.stepDefinitions;
 
 import ca.bc.gov.open.jag.efiling.TestConfig;
-import ca.bc.gov.open.jag.efiling.helpers.PayloadHelper;
+import ca.bc.gov.open.jag.efiling.helpers.SubmissionHelper;
 import ca.bc.gov.open.jag.efiling.models.UserIdentity;
 import ca.bc.gov.open.jag.efiling.services.OauthService;
+import ca.bc.gov.open.jag.efiling.services.SubmissionService;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.spring.CucumberContextConfiguration;
-import io.restassured.RestAssured;
-import io.restassured.builder.MultiPartSpecBuilder;
-import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.MultiPartSpecification;
-import io.restassured.specification.RequestSpecification;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Assert;
@@ -35,21 +32,21 @@ public class GenerateUrlSD {
 
 
     private final OauthService oauthService;
+    private final SubmissionService submissionService;
 
-    private static final String X_USER_ID = "X-User-Id";
     private static final String TEST_DOCUMENT_PDF = "test-document.pdf";
-    private static final String X_TRANSACTION_ID = "X-Transaction-Id";
 
     private UUID actualTransactionId;
     private Response actualDocumentResponse;
     private Response actualGenerateUrlResponse;
-    private UUID actualSubmissionId;
+    private String actualSubmissionId;
     private UserIdentity actualUserIdentity;
 
     public Logger logger = LogManager.getLogger(GenerateUrlAndSubmissionTest.class);
 
-    public GenerateUrlSD(OauthService oauthService) {
+    public GenerateUrlSD(OauthService oauthService, SubmissionService submissionService) {
         this.oauthService = oauthService;
+        this.submissionService = submissionService;
         actualTransactionId = UUID.randomUUID();
     }
 
@@ -67,22 +64,14 @@ public class GenerateUrlSD {
         File resource = new ClassPathResource(
                 "data/test-document.pdf").getFile();
 
-        MultiPartSpecification spec = new MultiPartSpecBuilder(resource).
-                fileName(TEST_DOCUMENT_PDF).
-                controlName("files").
-                mimeType("text/application-pdf").
-                build();
+        MultiPartSpecification fileSpec = SubmissionHelper.fileSpecBuilder(resource,TEST_DOCUMENT_PDF, "text/application.pdf");
 
-        RequestSpecification request = RestAssured.given().auth().preemptive().oauth2(actualUserIdentity.getAccessToken())
-                .header(X_TRANSACTION_ID, actualTransactionId)
-                .header(X_USER_ID, actualUserIdentity.getUniversalId())
-                .multiPart(spec);
+        actualDocumentResponse = submissionService.documentUploadResponse(actualTransactionId, actualUserIdentity.getUniversalId(),
+                                                                    actualUserIdentity.getAccessToken(), fileSpec );
 
-        actualDocumentResponse = request.when().post("http://localhost:8080/submission/documents").then()
-                .extract().response();
 
-        logger.info("Api response status code: {}", actualDocumentResponse.getStatusCode());
-        logger.info("Api response: {}", actualDocumentResponse.asString());
+       logger.info("Api response status code: {}", actualDocumentResponse.getStatusCode());
+       logger.info("Api response: {}", actualDocumentResponse.asString());
 
     }
 
@@ -93,7 +82,8 @@ public class GenerateUrlSD {
 
         JsonPath jsonPath = new JsonPath(actualDocumentResponse.asString());
         Assert.assertEquals("File Received not don't match", new BigDecimal(1), new BigDecimal(jsonPath.get("received").toString()));
-        actualSubmissionId = UUID.fromString(jsonPath.getString("submissionId"));
+
+        actualSubmissionId = submissionService.getSubmissionId(actualDocumentResponse);
 
         logger.info("Response match requirements");
 
@@ -102,26 +92,11 @@ public class GenerateUrlSD {
     @And("user request a submission url")
     public void userRequestASubmissionUrl() {
 
-        logger.info("Submitting generate url request");
-
-        RequestSpecification request = RestAssured
-                .given()
-                .auth()
-                .preemptive()
-                .oauth2(actualUserIdentity.getAccessToken())
-                .contentType(ContentType.JSON)
-                .header(X_TRANSACTION_ID, actualTransactionId)
-                .header(X_USER_ID, actualUserIdentity.getUniversalId())
-                .body(PayloadHelper.generateUrlPayload(TEST_DOCUMENT_PDF));
-
-        actualGenerateUrlResponse = request
-                .when()
-                .post(MessageFormat.format( "http://localhost:8080/submission/{0}/generateUrl", actualSubmissionId))
-                .then()
-                .extract()
-                .response();
+        actualGenerateUrlResponse = submissionService.generateUrlResponse(actualTransactionId, actualUserIdentity.getUniversalId(),
+                                                                                 actualUserIdentity.getAccessToken(),TEST_DOCUMENT_PDF, actualSubmissionId);
 
         logger.info("Api response status code: {}", actualGenerateUrlResponse.getStatusCode());
+
         logger.info("Api response: {}", actualGenerateUrlResponse.asString());
 
     }
