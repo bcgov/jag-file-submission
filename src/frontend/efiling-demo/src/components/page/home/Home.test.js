cfa216b0bedddea181164a176738d8ddb22546ec
yousiefc <redykulous96@gmail.com>
2021-01-19 10:36:47 -0800

    removed document matching test (not relevant with updates)

--- src/frontend/efiling-demo/src/components/page/home/Home.test.js
+++ src/frontend/efiling-demo/src/components/page/home/Home.test.js
@@ -205,7 +205,8 @@ describe("Home", () => {
     expect(window.open).toHaveBeenCalledWith("example.com", "_self");
   });
 
-  test("uploading document with incorrect name matching filing package sets error", async () => {
+  //This test is not relevant anymore as document names are no longer required in the filing package (JSON)
+  /*test("uploading document with incorrect name matching filing package sets error", async () => {
     mock
       .onPost(
         "apikeycloakexample.com/realms/apiRealm/protocol/openid-connect/token"
@@ -251,5 +252,5 @@ describe("Home", () => {
     );
 
     expect(error).toBeInTheDocument();
-  });
+  });*/
 });
