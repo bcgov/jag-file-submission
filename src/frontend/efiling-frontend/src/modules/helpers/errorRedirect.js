3e75fa29132e83b212fc2571b8ec649136070d04
Kevin Neufeld <Kevin.Neufeld@nttdata.com>
2021-01-26 12:36:25 -0800

    simplified errorRedirect logic

--- src/frontend/efiling-frontend/src/modules/helpers/errorRedirect.js
+++ src/frontend/efiling-frontend/src/modules/helpers/errorRedirect.js
@@ -1,28 +1,30 @@
+function getDefaultErrorStatus(error) {
+  if (error.status) {
+    return error.status;
+  }
+  if (error.response && error.response.status) {
+    return error.response.status;
+  }
+  return "unknown";
+}
+
+function getDefaultErrorMsg(error) {
+  if (error.response && error.response.data && error.response.data.message) {
+    return error.response.data.message;
+  }
+  if (error.message) {
+    return error.message;
+  }
+  return "There was an error with your submission.";
+}
+
 export function errorRedirect(errorUrl, error) {
   if (errorUrl && error) {
-    let errorStatus;
-    if (error.status) {
-      errorStatus = error.status;
-    } else if (error.response && error.response.status) {
-      errorStatus = error.response.status;
-    } else {
-      errorStatus = "unknown";
-    }
-
-    let errorMessage;
-    if (error.response && error.response.data && error.response.data.message) {
-      errorMessage = error.response.data.message;
-    } else if (error.message) {
-      errorMessage = error.message;
-    } else {
-      errorMessage = "There was an error with your submission.";
-    }
+    const status = getDefaultErrorStatus(error);
+    const msg = getDefaultErrorMsg(error);
 
     sessionStorage.setItem("validExit", true);
-    return window.open(
-      `${errorUrl}?status=${errorStatus}&message=${errorMessage}`,
-      "_self"
-    );
+    return window.open(`${errorUrl}?status=${status}&message=${msg}`, "_self");
   }
 
   return null;
