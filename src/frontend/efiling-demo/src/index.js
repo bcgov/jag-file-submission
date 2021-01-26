b4459a3354aac75873537ed280cf24517b1c2fee
alex <alexandre.joyeux@nttdata.com>
2021-01-26 10:16:36 -0800

    implementing environment variables

--- src/frontend/efiling-demo/src/index.js
+++ src/frontend/efiling-demo/src/index.js
@@ -9,8 +9,8 @@ import "@bcgov/bootstrap-theme/dist/css/bootstrap-theme.min.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-axios.defaults.baseURL = window.REACT_APP_API_BASE_URL
-  ? window.REACT_APP_API_BASE_URL
+axios.defaults.baseURL = window._env_.REACT_APP_API_BASE_URL
+  ? window._env_.REACT_APP_API_BASE_URLREACT_APP_API_BASE_URL
   : process.env.REACT_APP_API_BASE_URL;
 
 ReactDOM.render(
