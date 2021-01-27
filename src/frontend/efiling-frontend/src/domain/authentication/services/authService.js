caf561a244728ea6fb48913e49645ca8f2c7685a
alex <alexandre.joyeux@nttdata.com>
2021-01-27 09:51:57 -0800

    fix the lint

--- src/frontend/efiling-frontend/src/domain/authentication/services/authService.js
+++ src/frontend/efiling-frontend/src/domain/authentication/services/authService.js
@@ -8,25 +8,7 @@
  * Using the current OIDC token, request a broker token directly from Keycloak
  */
 export const getKeycloakBrokerToken = () => {
-  // const oidcToken = localStorage.getItem("jwt");
-
-  // const response = await fetch(
-  //   `${KEYCLOAK.url}/realms/${KEYCLOAK.realm}/broker/bcsc/token`,
-  //   {
-  //     method: "GET",
-  //     headers: new Headers({
-  //       "Content-type": "application/json",
-  //       Authorization: `Bearer ${oidcToken}`,
-  //     }),
-  //   }
-  // );
-  // if (!response.ok) {
-  //   throw new Error(`HTTP error, status: ${response.status}`);
-  // }
-  // return response.json().then((data) => data.access_token);
-
   console.log("whynot");
-
   return "yeah";
 };
 
@@ -35,19 +17,6 @@ export const getKeycloakBrokerToken = () => {
  */
 export const getBCSCUserInfoJWT = (brokerToken) => {
   console.log(brokerToken);
-
-  // const response = await fetch(`${BCSC_USERINFO_URL}`, {
-  //   method: "GET",
-  //   headers: new Headers({
-  //     Authorization: `Bearer ${brokerToken}`,
-  //   }),
-  // });
-  // if (!response.ok) {
-  //   throw new Error(`HTTP error, status: ${response.status}`);
-  // }
-
-  console.log("whynot");
-
   return "yahooo";
 };
 
@@ -61,16 +30,6 @@ export const getBCSCUserInfoJWT = (brokerToken) => {
  * }
  */
 export const getBCSCUserInfo = () => {
-  // const brokerToken = await getKeycloakBrokerToken();
-  // const bcscToken = await getBCSCUserInfoJWT(brokerToken);
-
-  // const userInfo = decodeJWT(bcscToken);
-  // const firstName = userInfo.given_name ? userInfo.given_name : "";
-  // let middleName = userInfo.given_names ? userInfo.given_names : "";
-  // middleName = middleName.replace(firstName, "").trim();
-  // const lastName = userInfo.family_name ? userInfo.family_name : "";
-
   console.log("whynot");
-
   return { one: "yeah", 2: "yo", 3: "eheheh" };
 };
