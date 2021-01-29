e3af8444eccf8f921339878a55d0f771421b7c7a
C <37885787+yousiefc@users.noreply.github.com>
2021-01-29 03:54:09 +0000

    Update API calls to match backend changes

--- src/frontend/efiling-frontend/src/components/page/package-review/PackageReview.js
+++ src/frontend/efiling-frontend/src/components/page/package-review/PackageReview.js
@@ -58,7 +58,7 @@ export default function PackageReview({ page: { header, packageId } }) {
 
   useEffect(() => {
     axios
-      .get(`filingpackage/${packageId}`)
+      .get(`filingpackages/${packageId}`)
       .then((response) => {
         try {
           const packageNo = response.data.packageNumber || "";
