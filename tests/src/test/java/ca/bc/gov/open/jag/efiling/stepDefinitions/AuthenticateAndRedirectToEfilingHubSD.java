d03875d2ce167451d34782d5cb4ef147127f32dc
Karthikeyan <karthikmsqa@gmail.com>
2021-02-02 13:23:10 -0800

    added missing class

--- tests/src/test/java/ca/bc/gov/open/jag/efiling/stepDefinitions/AuthenticateAndRedirectToEfilingHubSD.java
+++ tests/src/test/java/ca/bc/gov/open/jag/efiling/stepDefinitions/AuthenticateAndRedirectToEfilingHubSD.java
@@ -1,16 +1,23 @@
 package ca.bc.gov.open.jag.efiling.stepDefinitions;
 
 import ca.bc.gov.open.jag.efiling.page.AuthenticationPage;
+import ca.bc.gov.open.jag.efiling.page.PackageConfirmationPage;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
 
 import java.io.IOException;
 
 
-public class redirectToEfilingHubSD {
+public class AuthenticateAndRedirectToEfilingHubSD {
 
     private AuthenticationPage authenticationPage;
+    private PackageConfirmationPage packageConfirmationPage;
+    private static final String EFILE_SUBMISSION_PAGE_TITLE = "E-File submission";
+    private static final String INITIAL_DOCUMENT_NAME = "test-document.pdf";
 
     // EFileSubmissionPage eFileSubmissionPage;
     /*PackageConfirmationPage packageConfirmationPage;
@@ -19,10 +26,11 @@ public class redirectToEfilingHubSD {
     private static final String SECOND_PDF_PATH = "/src/test/java/testdatasource/test-document-2.pdf";
     private final List<String> expectedUploadedFilesList = ImmutableList.of("data/test-document.pdf", "test-document-2.pdf");
 */
-    Logger log = LogManager.getLogger(redirectToEfilingHubSD.class);
+    Logger log = LogManager.getLogger(AuthenticateAndRedirectToEfilingHubSD.class);
 
-    public redirectToEfilingHubSD(AuthenticationPage authenticationPage) {
+    public AuthenticateAndRedirectToEfilingHubSD(AuthenticationPage authenticationPage, PackageConfirmationPage packageConfirmationPage) {
         this.authenticationPage = authenticationPage;
+        this.packageConfirmationPage = packageConfirmationPage;
     }
 
     @Given("user is on the eFiling submission page")
@@ -33,27 +41,19 @@ public class redirectToEfilingHubSD {
         log.info("user is authenticated with keycloak");
     }
 
-/*        try {
-            GenerateUrlHelper generateUrlHelper = new GenerateUrlHelper();
-            String respUrl = generateUrlHelper.getGeneratedUrl();
-
-            driver.get(respUrl);
-            log.info("EFiling submission page url is accessed successfully");
-
-            AuthenticationPage authenticationPage = new AuthenticationPage(driver);
-            if(System.getProperty("ENV").equals("demo")) {
-                authenticationPage.clickBceid();
-            }
-            Thread.sleep(4000L);
-            authenticationPage.signInWithBceid(System.getProperty("BCEID_USERNAME"), System.getProperty("BCEID_PASSWORD"));
-
-
-        } catch (TimeoutException | InterruptedException tx) {
-            log.info("Efiling hub page is not displayed");
-        }
-        Assert.assertEquals(EFILE_SUBMISSION_PAGE_TITLE, eFileSubmissionPage.verifyEfilingPageTitle());
+    @Then("Package information is displayed")
+    public void verifyPackageInformation() {
+        Assert.assertEquals(EFILE_SUBMISSION_PAGE_TITLE, this.packageConfirmationPage.verifyPageTitle());
         log.info("Efiling submission page title is verified");
-    }*/
 
+        Assert.assertEquals(INITIAL_DOCUMENT_NAME, this.packageConfirmationPage.getInitialDocumentName());
+        log.info("Actual document name matches the uploaded document name");
 
+    }
+
+    @And("continue to payment button is enabled")
+    public void verifyContinueToPaymentButtonIsEnabled() {
+        Assert.assertTrue(this.packageConfirmationPage.verifyContinuePaymentBtnIsEnabled());
+        log.info("Continue payment button is enabled");
+    }
 }
