56799030120e258e0cac0e07c1ce98a644ecf16f
alex <alexandre.joyeux@nttdata.com>
2021-02-12 08:44:11 -0800

    implement frontend changes related to api change

---
+++ src/frontend/efiling-frontend/src/domain/documents/DocumentService.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+/**
+ * Retrieves an array of document types
+ */
+export const getDocumentTypes = async (courtLevel, courtClassification) => {
+  const params = {
+    courtLevel,
+    courtClassification,
+  };
+
+  const response = await axios.get(`/documents/types`, { params });
+
+  return response.data.documentTypes;
+};
