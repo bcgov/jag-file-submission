de3e7739412c9ea800caba7a9f9af80460b73b55
TayGov <taylor.clausen@gov.bc.ca>
2021-02-16 12:59:39 -0800

    Adding log

--- src/backend/efiling-reviewer-api/src/main/java/ca/bc/gov/open/jag/efilingreviewerapi/extract/DocumentsApiDelegateImpl.java
+++ src/backend/efiling-reviewer-api/src/main/java/ca/bc/gov/open/jag/efilingreviewerapi/extract/DocumentsApiDelegateImpl.java
@@ -1,6 +1,8 @@
 package ca.bc.gov.open.jag.efilingreviewerapi.extract;
 
 import ca.bc.gov.open.jag.efilingreviewerapi.api.DocumentsApiDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -8,8 +10,13 @@ import java.util.UUID;
 
 public class DocumentsApiDelegateImpl implements DocumentsApiDelegate {
 
+    Logger logger = LoggerFactory.getLogger(DocumentsApiDelegateImpl.class);
+
     @Override
     public ResponseEntity<Object> extractDocumentFormData(UUID xTransactionId, String xDocumentType, MultipartFile file) {
+
+        logger.info("document extract request received");
+
         return ResponseEntity.ok("Hello");
     }
 }
