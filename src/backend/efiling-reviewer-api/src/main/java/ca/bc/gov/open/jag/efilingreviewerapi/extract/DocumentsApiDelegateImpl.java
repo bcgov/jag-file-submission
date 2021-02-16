a321951c2dcd2a0b72e744a28f95630306f7e11c
TayGov <taylor.clausen@gov.bc.ca>
2021-02-16 11:53:41 -0800

    DOH!

--- src/backend/efiling-reviewer-api/src/main/java/ca/bc/gov/open/jag/efilingreviewerapi/extract/DocumentsApiDelegateImpl.java
+++ src/backend/efiling-reviewer-api/src/main/java/ca/bc/gov/open/jag/efilingreviewerapi/extract/DocumentsApiDelegateImpl.java
@@ -7,6 +7,10 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.UUID;
 
 public class DocumentsApiDelegateImpl implements DocumentsApiDelegate {
+
+
+
+
     @Override
     public ResponseEntity<Object> extractDocumentFormData(UUID xTransactionId, String xDocumentType, MultipartFile file) {
         return ResponseEntity.ok("Hello");
