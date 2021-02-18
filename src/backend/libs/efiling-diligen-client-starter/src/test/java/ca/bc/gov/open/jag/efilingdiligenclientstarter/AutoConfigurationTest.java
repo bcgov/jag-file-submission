89eb161f87fc9c72bd2ebd7c28ac4f42d0f90b71
TayGov <taylor.clausen@gov.bc.ca>
2021-02-18 10:25:46 -0800

    Add beans to test

--- src/backend/libs/efiling-diligen-client-starter/src/test/java/ca/bc/gov/open/jag/efilingdiligenclientstarter/AutoConfigurationTest.java
+++ src/backend/libs/efiling-diligen-client-starter/src/test/java/ca/bc/gov/open/jag/efilingdiligenclientstarter/AutoConfigurationTest.java
@@ -1,12 +1,15 @@
 package ca.bc.gov.open.jag.efilingdiligenclientstarter;
 
+import ca.bc.gov.open.efilingdiligenclient.diligen.DiligenAuthService;
 import ca.bc.gov.open.efilingdiligenclient.diligen.DiligenProperties;
+import ca.bc.gov.open.efilingdiligenclient.diligen.DiligenService;
 import ca.bc.gov.open.jag.efilingdiligenclient.api.HealthCheckApi;
 import ca.bc.gov.open.jag.efilingdiligenclient.api.handler.ApiClient;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.web.client.RestTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -33,6 +36,9 @@ public class AutoConfigurationTest {
             assertThat(it).hasSingleBean(ApiClient.class);
             assertThat(it).hasSingleBean(HealthCheckApi.class);
             assertThat(it).doesNotHaveBean(DiligenHealthIndicator.class);
+            assertThat(it).hasSingleBean(RestTemplate.class);
+            assertThat(it).hasSingleBean(DiligenAuthService.class);
+            assertThat(it).hasSingleBean(DiligenService.class);
         });
 
     }
