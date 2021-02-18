8e08c48fa5d9364184c5768890b834cf3779dd9e
TayGov <taylor.clausen@gov.bc.ca>
2021-02-18 09:03:09 -0800

    Inject restemplate

--- src/backend/efiling-reviewer-api/src/main/java/ca/bc/gov/open/jag/efilingreviewerapi/extract/DocumentConfig.java
+++ src/backend/efiling-reviewer-api/src/main/java/ca/bc/gov/open/jag/efilingreviewerapi/extract/DocumentConfig.java
@@ -2,9 +2,13 @@ package ca.bc.gov.open.jag.efilingreviewerapi.extract;
 
 import ca.bc.gov.open.efilingdiligenclient.diligen.DiligenAuthService;
 import ca.bc.gov.open.jag.efilingdiligenclientstarter.DiligenProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+@Configuration
+@EnableConfigurationProperties(DiligenProperties.class)
 public class DocumentConfig {
 
     private final DiligenProperties diligenProperties;
@@ -14,8 +18,13 @@ public class DocumentConfig {
     }
 
     @Bean
-    public DiligenService diligenService(DiligenAuthService diligenAuthService) {
-        return new DiligenServiceImpl(new RestTemplate(), diligenProperties, diligenAuthService);
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public DiligenService diligenService(DiligenAuthService diligenAuthService, RestTemplate restTemplate) {
+        return new DiligenServiceImpl(restTemplate, diligenProperties, diligenAuthService);
     }
 
 }
