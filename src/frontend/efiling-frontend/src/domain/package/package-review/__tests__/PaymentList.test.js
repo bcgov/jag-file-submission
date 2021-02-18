dd252138d7b4916a1029137669ab441893168f1d
Celina <yousiefc@pm.me>
2021-02-17 17:30:44 -0800

    Payment List

--- src/frontend/efiling-frontend/src/domain/package/package-review/__tests__/PaymentList.test.js
+++ src/frontend/efiling-frontend/src/domain/package/package-review/__tests__/PaymentList.test.js
@@ -7,7 +7,38 @@ describe("PartyList Component", () => {
 
   test("Matches snapshot", () => {
     const payments = [
-      
+      {
+        feeExempt: false,
+        paymentCategory: 1,
+        processedAmount: 7,
+        submittedAmount: 7,
+        serviceIdentifier: 0,
+        transactionDate: null
+      },
+      {
+        feeExempt: true,
+        paymentCategory: 3,
+        processedAmount: 7,
+        submittedAmount: null,
+        serviceIdentifier: 0,
+        transactionDate: null
+      },
+      {
+        feeExempt: false,
+        paymentCategory: 3,
+        processedAmount: 7,
+        submittedAmount: 7,
+        serviceIdentifier: 0,
+        transactionDate: null
+      },
+      {
+        feeExempt: false,
+        paymentCategory: 3,
+        processedAmount: null,
+        submittedAmount: 7,
+        serviceIdentifier: 0,
+        transactionDate: null
+      }
     ];
     const { asFragment } = render(<PaymentList payments={payments} />);
     waitFor(() => {});
