5c9debcfe151e49acc502e6ca86196e7d5f64f55
Karthikeyan <karthikmsqa@gmail.com>
2021-02-18 08:41:44 -0800

    Removed logging filter

--- tests/src/test/java/ca/bc/gov/open/jag/efiling/services/DocumentService.java
+++ tests/src/test/java/ca/bc/gov/open/jag/efiling/services/DocumentService.java
@@ -1,8 +1,6 @@
 package ca.bc.gov.open.jag.efiling.services;
 
 import io.restassured.RestAssured;
-import io.restassured.filter.log.RequestLoggingFilter;
-import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,8 +13,6 @@ public class DocumentService {
     private String eFilingHost;
 
     public Response getDocumentTypesResponse(String accessToken, String courtLevel, String courtClassification) {
-
-        RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
 
         RequestSpecification request = RestAssured
                 .given()
