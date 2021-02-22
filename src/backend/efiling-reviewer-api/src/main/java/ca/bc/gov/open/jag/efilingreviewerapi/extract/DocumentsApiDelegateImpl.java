43b8b98a74ab8be1a4fd8a4eb3992471bfe8d064
TayGov <taylor.clausen@gov.bc.ca>
2021-02-22 11:28:25 -0800

    Adding file scans

--- src/backend/efiling-reviewer-api/src/main/java/ca/bc/gov/open/jag/efilingreviewerapi/extract/DocumentsApiDelegateImpl.java
+++ src/backend/efiling-reviewer-api/src/main/java/ca/bc/gov/open/jag/efilingreviewerapi/extract/DocumentsApiDelegateImpl.java
@@ -1,17 +1,23 @@
 package ca.bc.gov.open.jag.efilingreviewerapi.extract;
 
 import ca.bc.gov.open.clamav.starter.ClamAvService;
+import ca.bc.gov.open.clamav.starter.VirusDetectedException;
 import ca.bc.gov.open.efilingdiligenclient.diligen.DiligenService;
+import ca.bc.gov.open.efilingdiligenclient.exception.DiligenDocumentException;
 import ca.bc.gov.open.jag.efilingreviewerapi.api.DocumentsApiDelegate;
 import ca.bc.gov.open.jag.efilingreviewerapi.api.model.Document;
 import ca.bc.gov.open.jag.efilingreviewerapi.api.model.DocumentExtractResponse;
 import ca.bc.gov.open.jag.efilingreviewerapi.api.model.Extract;
+import ca.bc.gov.open.jag.efilingreviewerapi.utils.TikaAnalysis;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.UUID;
 @Service
@@ -33,6 +39,17 @@ public class DocumentsApiDelegateImpl implements DocumentsApiDelegate {
 
         logger.info("document extract request received");
 
+        try {
+            clamAvService.scan(new ByteArrayInputStream(file.getBytes()));
+        } catch (VirusDetectedException e) {
+            throw new DiligenDocumentException("Virus found in document");
+        } catch (IOException e) {
+            throw new DiligenDocumentException("File is corrupt");
+        }
+
+        if (!TikaAnalysis.isPdf(file)) throw new DiligenDocumentException("Invalid file type");
+
+        return ResponseEntity.ok(diligenService.postDocument(xDocumentType, file));
         BigDecimal response = diligenService.postDocument(xDocumentType, file);
 
         DocumentExtractResponse documentExtractResponse = new DocumentExtractResponse();
