3c10db3555c299d1b052e7d77cff1c8a75fbdafc
Karthikeyan <karthikmsqa@gmail.com>
2021-02-23 12:17:21 -0800

    cc feedback private constructor

--- tests/src/test/java/ca/bc/gov/open/jag/efiling/helpers/DeleteFileHelper.java
+++ tests/src/test/java/ca/bc/gov/open/jag/efiling/helpers/DeleteFileHelper.java
@@ -5,6 +5,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class DeleteFileHelper {
+
+    private DeleteFileHelper() {
+        throw new IllegalStateException("Helper class");
+    }
+
     public static void deleteDownloadedFile(Path path) throws IOException {
         Files.delete(path);
     }
