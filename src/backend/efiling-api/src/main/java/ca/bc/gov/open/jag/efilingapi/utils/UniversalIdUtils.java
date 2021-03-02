1034858902b197c23b533e0dbed2264dc8d9f8b1
alex <alexandre.joyeux@nttdata.com>
2021-03-02 09:46:55 -0800

    fix pr feedback

--- src/backend/efiling-api/src/main/java/ca/bc/gov/open/jag/efilingapi/utils/UniversalIdUtils.java
+++ src/backend/efiling-api/src/main/java/ca/bc/gov/open/jag/efilingapi/utils/UniversalIdUtils.java
@@ -4,6 +4,8 @@ import org.apache.commons.lang3.StringUtils;
 
 public class UniversalIdUtils {
 
+    private UniversalIdUtils() {}
+
     public static String sanitizeUniversalId(String universalId) {
         return StringUtils.replace(universalId, "-", "").toUpperCase();
     }
