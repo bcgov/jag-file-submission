37ae838f0909db7d6fd625ce20c5d7a2632c0f7b
Karthikeyan <karthikmsqa@gmail.com>
2021-03-09 07:00:31 -0800

    download payment receipt impl

---
+++ tests/src/test/java/ca/bc/gov/open/jag/efiling/helpers/FileDownloadHelper.java
@@ -0,0 +1,25 @@
+package ca.bc.gov.open.jag.efiling.helpers;
+
+import ca.bc.gov.open.jag.efiling.error.EfilingTestException;
+
+import java.io.File;
+
+public class FileDownloadHelper {
+
+    public File downloadFile(String downloadedFilesPath) {
+
+        File folder = new File(downloadedFilesPath);
+        File[] listOfFiles = folder.listFiles();
+
+        File actualFile = null;
+
+        if (listOfFiles == null) throw new EfilingTestException("Downloaded file is not present");
+        for (File file : listOfFiles) {
+            if (file.isFile()) {
+                actualFile = file;
+                break;
+            }
+        }
+        return actualFile;
+    }
+}
