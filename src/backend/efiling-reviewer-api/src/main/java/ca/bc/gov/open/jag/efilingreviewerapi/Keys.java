726fd741dc332598747ddcc9126747074a80783b
TayGov <taylor.clausen@gov.bc.ca>
2021-03-10 10:03:30 -0800

    Add new status

--- src/backend/efiling-reviewer-api/src/main/java/ca/bc/gov/open/jag/efilingreviewerapi/Keys.java
+++ src/backend/efiling-reviewer-api/src/main/java/ca/bc/gov/open/jag/efilingreviewerapi/Keys.java
@@ -7,6 +7,6 @@ public class Keys {
     private Keys() {}
 
     public static final String PROCESSED_STATUS = "PROCESSED";
-    public static final List<String> ACCEPTED_STATUS = Arrays.asList("QUEUED_FOR_ML_ANALYSIS", "DONE_OCR_PROCESSING","QUEUED_FOR_OCR_PROCESSING", "QUEUED_FOR_PROCESSING");
+    public static final List<String> ACCEPTED_STATUS = Arrays.asList("QUEUED_FOR_ML_ANALYSIS", "DONE_OCR_PROCESSING","QUEUED_FOR_OCR_PROCESSING", "QUEUED_FOR_TRANSLATE", "QUEUED_FOR_PROCESSING");
 
 }
