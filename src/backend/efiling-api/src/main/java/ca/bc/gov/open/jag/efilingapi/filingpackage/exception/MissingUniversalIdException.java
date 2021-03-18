ba89fe510a90156e8df0b7376ec8d4ddc42ebd5b
TayGov <taylor.clausen@gov.bc.ca>
2021-03-18 11:39:07 -0700

    Remove unused

--- src/backend/efiling-api/src/main/java/ca/bc/gov/open/jag/efilingapi/filingpackage/exception/MissingUniversalIdException.java
+++
@@ -1,9 +0,0 @@
-package ca.bc.gov.open.jag.efilingapi.filingpackage.exception;
-
-public class MissingUniversalIdException extends RuntimeException {
-
-    public MissingUniversalIdException(String message) {
-        super(message);
-    }
-
-}
--- src/backend/efiling-api/src/main/java/ca/bc/gov/open/jag/efilingapi/filingpackage/exception/ReportNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package ca.bc.gov.open.jag.efilingapi.filingpackage.exception;
-
-public class ReportNotFoundException extends RuntimeException {
-
-    public ReportNotFoundException(String message) {
-        super(message);
-    }
-
-}
