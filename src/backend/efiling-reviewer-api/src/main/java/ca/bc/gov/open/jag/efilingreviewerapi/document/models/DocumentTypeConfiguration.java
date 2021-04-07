e800b7308d34f7aa49b1f5353a0b1e3ae09e8458
alex <alexandre.joyeux@nttdata.com>
2021-04-07 11:59:26 -0700

    Adding endpoint to post a document

--- src/backend/efiling-reviewer-api/src/main/java/ca/bc/gov/open/jag/efilingreviewerapi/document/models/DocumentTypeConfiguration.java
+++ src/backend/efiling-reviewer-api/src/main/java/ca/bc/gov/open/jag/efilingreviewerapi/document/models/DocumentTypeConfiguration.java
@@ -1,9 +1,13 @@
 package ca.bc.gov.open.jag.efilingreviewerapi.document.models;
 
 import ca.bc.gov.open.efilingdiligenclient.diligen.model.DocumentConfig;
+import ca.bc.gov.open.efilingdiligenclient.diligen.model.PropertyConfig;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.annotation.Id;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class DocumentTypeConfiguration {
@@ -67,6 +71,47 @@ public class DocumentTypeConfiguration {
         public Builder documentConfig(DocumentConfig documentConfig) {
             this.documentConfig = documentConfig;
             return this;
+        }
+
+        public Builder documentConfig(LinkedHashMap<String, Object> documentConfigMap) {
+
+            documentConfig = new DocumentConfig();
+
+            for(Map.Entry<String, Object> entry: documentConfigMap.entrySet()) {
+
+                if(StringUtils.equals("properties", entry.getKey())) {
+                    documentConfig.setProperties(convertProperty((LinkedHashMap<String, Object>) entry.getValue()));
+                }
+
+            }
+
+            return this;
+
+        }
+
+        private Map<String, PropertyConfig> convertProperty(LinkedHashMap<String, Object> propertyMap) {
+
+            Map<String, PropertyConfig> propertyConfigMap = new LinkedHashMap<>();
+
+            for(Map.Entry<String, Object> entry: propertyMap.entrySet()) {
+
+                PropertyConfig propertyConfig = new PropertyConfig();
+
+                LinkedHashMap<String, Object> propertyConfigMapInternal = (LinkedHashMap<String, Object>)entry.getValue();
+
+                propertyConfig.setType(propertyConfigMapInternal.get("type").toString());
+
+                if(StringUtils.equals("object", propertyConfig.getType())) {
+                    propertyConfig.setProperties(convertProperty((LinkedHashMap<String, Object>)propertyConfigMapInternal.get("properties")));
+                } else if (StringUtils.equals("string", propertyConfig.getType())) {
+                    propertyConfig.setFieldId(Integer.parseInt(propertyConfigMapInternal.get("fieldId").toString()));
+                }
+
+                propertyConfigMap.put(entry.getKey(), propertyConfig);
+
+            }
+
+            return propertyConfigMap;
         }
 
         public DocumentTypeConfiguration create() {
