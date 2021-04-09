0fad83e43600f456fcdf78a536363b33abc1d396
Kevin Neufeld <Kevin.Neufeld@nttdata.com>
2021-04-09 12:09:07 -0700

    import shared-components, add Header Footer

--- src/frontend/ai-reviewer-admin/src/App.js
+++ src/frontend/ai-reviewer-admin/src/App.js
@@ -1,9 +1,20 @@
+import { useHistory } from "react-router-dom";
+import { Header, Footer } from "shared-components";
 import './App.css';
 
 function App() {
+  
+  const header = {
+    name: "AI Reviewer Admin Client",
+    history: useHistory(),
+  };
+
   return (
-    <div className="App">
-    </div>
+    <main>
+      <Header header={header} />
+      Hello World!
+      <Footer />
+    </main>
   );
 }
 
--- src/frontend/ai-reviewer-admin/src/index.js
+++ src/frontend/ai-reviewer-admin/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import "@bcgov/bootstrap-theme/dist/css/bootstrap-theme.min.css";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
