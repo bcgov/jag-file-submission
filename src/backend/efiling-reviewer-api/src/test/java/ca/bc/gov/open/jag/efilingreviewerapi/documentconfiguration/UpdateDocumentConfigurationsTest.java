d658ac516c9b873858f0bdd7a82d04824391c60f
TayGov <taylor.clausen@gov.bc.ca>
2021-04-22 07:18:03 -0700

    Update tests

--- src/backend/efiling-reviewer-api/src/test/java/ca/bc/gov/open/jag/efilingreviewerapi/documentconfiguration/UpdateDocumentConfigurationsTest.java
+++ src/backend/efiling-reviewer-api/src/test/java/ca/bc/gov/open/jag/efilingreviewerapi/documentconfiguration/UpdateDocumentConfigurationsTest.java
@@ -16,15 +16,20 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.*;
+
+import static org.mockito.ArgumentMatchers.any;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UpdateDocumentConfigurationsTest {
     private static final String CASE_1 = "CASE1";
     private static final String CASE_2 = "CASE2";
-    public static final String DESCRIPTION = "DESCRIPTION";
+    private static final String CASE_3 = "CASE3";
+    private static final String DESCRIPTION = "DESCRIPTION";
+    private static final UUID CASE_1_ID = UUID.randomUUID();
+    private static final UUID CASE_2_ID = UUID.randomUUID();
+    private static final UUID CASE_3_ID = UUID.randomUUID();
+
     private DocumentConfigurationsApiDelegateImpl sut;
     @Mock
     private DocumentTypeConfigurationRepository documentTypeConfigurationRepositoryMock;
@@ -44,17 +49,25 @@ public class UpdateDocumentConfigurationsTest {
 
         documentTypeConfigurationList.add(documentTypeConfiguration);
 
-        Mockito.when(documentTypeConfigurationRepositoryMock.findAll()).thenReturn(documentTypeConfigurationList);
+        Mockito.when(documentTypeConfigurationRepositoryMock.findById(ArgumentMatchers.eq(CASE_1_ID))).thenReturn(Optional.of(documentTypeConfiguration));
+
+        Mockito.when(documentTypeConfigurationRepositoryMock.findById(ArgumentMatchers.eq(CASE_2_ID))).thenReturn(Optional.of(documentTypeConfiguration));
 
         Mockito.when(documentTypeConfigurationRepositoryMock.findByDocumentType(CASE_1)).thenReturn(documentTypeConfiguration);
+
+        Mockito.when(documentTypeConfigurationRepositoryMock.existsByDocumentTypeAndId(ArgumentMatchers.eq(CASE_3), ArgumentMatchers.eq(CASE_3_ID))).thenReturn(false);
+
+        Mockito.when(documentTypeConfigurationRepositoryMock.existsByDocumentTypeAndId(ArgumentMatchers.eq(CASE_1), ArgumentMatchers.eq(CASE_1_ID))).thenReturn(true);
+
+        Mockito.when(documentTypeConfigurationRepositoryMock.existsByDocumentTypeAndId(ArgumentMatchers.eq(CASE_2), ArgumentMatchers.eq(CASE_2_ID))).thenReturn(true);
 
         DocumentTypeConfiguration documentTypeConfiguration2 = DocumentTypeConfiguration
                 .builder()
                 .documentConfig(new DocumentConfig())
-                .documentType("CASE2")
+                .documentType("CASE1")
                 .create();
 
-        Mockito.doReturn(documentTypeConfiguration2).when(documentTypeConfigurationRepositoryMock).save(ArgumentMatchers.argThat(x -> x.getDocumentType().equals(CASE_2)));
+        Mockito.doReturn(documentTypeConfiguration2).when(documentTypeConfigurationRepositoryMock).save(any());
 
         DocumentTypeConfigurationMapper documentTypeConfigurationMapper = new DocumentTypeConfigurationMapperImpl();
         sut = new DocumentConfigurationsApiDelegateImpl(documentTypeConfigurationRepositoryMock, documentTypeConfigurationMapper);
@@ -62,15 +75,56 @@ public class UpdateDocumentConfigurationsTest {
     }
 
     @Test
-    @DisplayName("ok: POST /documentTypeConfigurations should create a new document type configuration")
-    public void withValidDocumentShouldCreateNewConfiguration() {
+    @DisplayName("ok: PUT /documentTypeConfigurations should update a document type configuration")
+    public void withValidDocumentShouldUpdateConfiguration() {
 
-        DocumentTypeConfigurationRequest documentTypeConfigurationRequest = new DocumentTypeConfigurationRequest();
+        ca.bc.gov.open.jag.efilingreviewerapi.api.model.DocumentTypeConfiguration documentTypeConfigurationRequest = new ca.bc.gov.open.jag.efilingreviewerapi.api.model.DocumentTypeConfiguration();
+        DocumentType documentType = new DocumentType();
+        documentType.setType(CASE_1);
+        documentType.setDescription(DESCRIPTION);
+        documentTypeConfigurationRequest.setDocumentType(documentType);
+        documentTypeConfigurationRequest.setProjectId(1);
+        documentTypeConfigurationRequest.setId(CASE_1_ID);
+
+        LinkedHashMap<String, Object> testProperty = new LinkedHashMap<>();
+
+        testProperty.put("type", "string");
+        testProperty.put("fieldId", "230");
+
+        LinkedHashMap<String, Object> courtProperties = new LinkedHashMap<>();
+        courtProperties.put("test", testProperty);
+
+        LinkedHashMap<String, Object> courtProperty = new LinkedHashMap<>();
+        courtProperty.put("type", "object");
+        courtProperty.put("properties", courtProperties);
+
+        LinkedHashMap<String, Object> documentProperties = new LinkedHashMap<>();
+        documentProperties.put("court", courtProperty);
+
+        LinkedHashMap<String, Object> documentConfig = new LinkedHashMap<>();
+        documentConfig.put("properties", documentProperties);
+
+        documentTypeConfigurationRequest.setDocumentConfig(documentConfig);
+
+        ResponseEntity<ca.bc.gov.open.jag.efilingreviewerapi.api.model.DocumentTypeConfiguration> response = sut.updateDocumentType(documentTypeConfigurationRequest);
+
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        Assertions.assertEquals("CASE1", response.getBody().getDocumentType().getType());
+
+    }
+
+    @Test
+    @DisplayName("Error: PUT /documentTypeConfigurations should return bad request if the document type doesn't exists")
+    public void withNonExistingDocumentShouldReturnError() {
+
+        ca.bc.gov.open.jag.efilingreviewerapi.api.model.DocumentTypeConfiguration documentTypeConfigurationRequest = new ca.bc.gov.open.jag.efilingreviewerapi.api.model.DocumentTypeConfiguration();
         DocumentType documentType = new DocumentType();
         documentType.setType(CASE_2);
         documentType.setDescription(DESCRIPTION);
         documentTypeConfigurationRequest.setDocumentType(documentType);
         documentTypeConfigurationRequest.setProjectId(1);
+        documentTypeConfigurationRequest.setId(CASE_2_ID);
 
         LinkedHashMap<String, Object> testProperty = new LinkedHashMap<>();
 
@@ -92,22 +146,21 @@ public class UpdateDocumentConfigurationsTest {
 
         documentTypeConfigurationRequest.setDocumentConfig(documentConfig);
 
-        ResponseEntity<ca.bc.gov.open.jag.efilingreviewerapi.api.model.DocumentTypeConfiguration> response = sut.createDocumentTypeConfiguration(documentTypeConfigurationRequest);
-
-        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
-
-        Assertions.assertEquals("CASE2", response.getBody().getDocumentType().getType());
+        Assertions.assertThrows(AiReviewerDocumentTypeConfigurationException.class, () -> sut.updateDocumentType(documentTypeConfigurationRequest));
 
     }
 
     @Test
-    @DisplayName("Error: POST /documentTypeConfigurations should return bad request if the document type already exists")
-    public void withExistingDocumentShouldReturnBadRequest() {
+    @DisplayName("Error: PUT /documentTypeConfigurations should return bad request if the document type doesn't exists")
+    public void withMismatchingDocumentTypeDocumentShouldReturnError() {
 
-        DocumentTypeConfigurationRequest documentTypeConfigurationRequest = new DocumentTypeConfigurationRequest();
+        ca.bc.gov.open.jag.efilingreviewerapi.api.model.DocumentTypeConfiguration documentTypeConfigurationRequest = new ca.bc.gov.open.jag.efilingreviewerapi.api.model.DocumentTypeConfiguration();
         DocumentType documentType = new DocumentType();
-        documentType.setType(CASE_1);
+        documentType.setType(CASE_3);
+        documentType.setDescription(DESCRIPTION);
         documentTypeConfigurationRequest.setDocumentType(documentType);
+        documentTypeConfigurationRequest.setProjectId(1);
+        documentTypeConfigurationRequest.setId(CASE_3_ID);
 
         LinkedHashMap<String, Object> testProperty = new LinkedHashMap<>();
 
@@ -129,7 +182,8 @@ public class UpdateDocumentConfigurationsTest {
 
         documentTypeConfigurationRequest.setDocumentConfig(documentConfig);
 
-        Assertions.assertThrows(AiReviewerDocumentTypeConfigurationException.class, () -> sut.createDocumentTypeConfiguration(documentTypeConfigurationRequest));
+        Assertions.assertThrows(AiReviewerDocumentTypeConfigurationException.class, () -> sut.updateDocumentType(documentTypeConfigurationRequest));
 
     }
+
 }
