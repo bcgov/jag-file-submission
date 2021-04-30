7376e910d5bbaf18900144341f0d934f99ae28c6
Kevin Neufeld <Kevin.Neufeld@nttdata.com>
2021-04-30 08:34:35 -0700

    FLA-1098 implement auditing

---
+++ src/backend/efiling-reviewer-api/src/main/java/ca/bc/gov/open/jag/efilingreviewerapi/document/models/Auditable.java
@@ -0,0 +1,49 @@
+package ca.bc.gov.open.jag.efilingreviewerapi.document.models;
+
+import java.util.Date;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.Version;
+
+/**
+ * An abstract Auditable class that auto-populates <code>createDate</code>,
+ * <code>modifiedDate</code>, and <code>version</code> fields. Class need only
+ * to extend this class to add auditing fields to a model object.
+ */
+public abstract class Auditable {
+
+	@CreatedDate
+	protected Date createdDate;
+
+	@LastModifiedDate
+	protected Date modifiedDate;
+
+	@Version
+	private Integer version;
+
+	public Date getCreatedDate() {
+		return createdDate == null ? new Date() : createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Date getModifiedDate() {
+		return modifiedDate;
+	}
+
+	public void setModifiedDate(Date modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+
+	public Integer getVersion() {
+		return version;
+	}
+
+	public void setVersion(Integer version) {
+		this.version = version;
+	}
+
+}
---
+++ src/backend/efiling-reviewer-api/src/main/java/ca/bc/gov/open/jag/efilingreviewerapi/document/models/DocumentTypeConfigurationAudit.java
@@ -0,0 +1,52 @@
+package ca.bc.gov.open.jag.efilingreviewerapi.document.models;
+
+import java.util.Date;
+import java.util.UUID;
+
+import org.springframework.data.annotation.Id;
+
+public class DocumentTypeConfigurationAudit {
+
+	@Id
+	private UUID id;
+	private DocumentTypeConfiguration DocumentTypeConfiguration;
+	private AuditAction auditAction;
+	private Date auditDate;
+
+	public DocumentTypeConfigurationAudit() {
+		id = UUID.randomUUID();
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public UUID getId() {
+		return id;
+	}
+	
+	public DocumentTypeConfiguration getDocumentTypeConfiguration() {
+		return DocumentTypeConfiguration;
+	}
+	
+	public void setDocumentTypeConfiguration(DocumentTypeConfiguration documentTypeConfiguration) {
+		DocumentTypeConfiguration = documentTypeConfiguration;
+	}
+
+	public AuditAction getAuditAction() {
+		return auditAction;
+	}
+
+	public void setAuditAction(AuditAction auditAction) {
+		this.auditAction = auditAction;
+	}
+
+	public Date getAuditDate() {
+		return auditDate;
+	}
+
+	public void setAuditDate(Date auditDate) {
+		this.auditDate = auditDate;
+	}
+
+}
