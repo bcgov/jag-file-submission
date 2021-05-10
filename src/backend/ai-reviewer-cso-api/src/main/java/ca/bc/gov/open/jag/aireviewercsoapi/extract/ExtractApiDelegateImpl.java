3f29027a015c32a6088b0d977b913672b4d6d4bc
TayGov <taylor.clausen@gov.bc.ca>
2021-05-10 11:52:10 -0700

    Fix spelling error

--- src/backend/ai-reviewer-cso-api/src/main/java/ca/bc/gov/open/jag/aireviewercsoapi/extract/ExtractApiDelegateImpl.java
+++ src/backend/ai-reviewer-cso-api/src/main/java/ca/bc/gov/open/jag/aireviewercsoapi/extract/ExtractApiDelegateImpl.java
@@ -1,7 +1,7 @@
 package ca.bc.gov.open.jag.aireviewercsoapi.extract;
 
 import ca.bc.gov.open.jag.aireviewercsoapi.api.ExtractApiDelegate;
-import ca.bc.gov.open.jag.aireviewercsoapi.api.model.ExtracNotification;
+import ca.bc.gov.open.jag.aireviewercsoapi.api.model.ExtractNotification;
 import org.springframework.http.ResponseEntity;
 
 import java.util.UUID;
@@ -9,7 +9,7 @@ import java.util.UUID;
 public class ExtractApiDelegateImpl implements ExtractApiDelegate {
 
     @Override
-    public ResponseEntity<Void> extractNotification(UUID xTransactionId, ExtracNotification extracNotification) {
+    public ResponseEntity<Void> extractNotification(UUID xTransactionId, ExtractNotification extractNotification) {
         return ResponseEntity.noContent().build();
     }
 }
