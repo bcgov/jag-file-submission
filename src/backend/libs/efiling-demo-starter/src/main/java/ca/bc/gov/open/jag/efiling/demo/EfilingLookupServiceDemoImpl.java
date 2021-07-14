82c5358bba3c031d9d481c647168f8dcde62cefd
TayGov <taylor.clausen@gov.bc.ca>
2021-07-14 11:27:52 -0700

    Add demo

--- src/backend/libs/efiling-demo-starter/src/main/java/ca/bc/gov/open/jag/efiling/demo/EfilingLookupServiceDemoImpl.java
+++ src/backend/libs/efiling-demo-starter/src/main/java/ca/bc/gov/open/jag/efiling/demo/EfilingLookupServiceDemoImpl.java
@@ -3,12 +3,21 @@ package ca.bc.gov.open.jag.efiling.demo;
 import ca.bc.gov.open.jag.efilingcommons.model.ServiceFees;
 import ca.bc.gov.open.jag.efilingcommons.model.SubmissionFeeRequest;
 import ca.bc.gov.open.jag.efilingcommons.service.EfilingLookupService;
+import ca.bc.gov.open.jag.efilingcommons.submission.models.LookupItem;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class EfilingLookupServiceDemoImpl implements EfilingLookupService {
+
+    @Override
+    public List<LookupItem> getCountries() {
+        return Arrays.asList(
+                LookupItem.builder().code("CDN").description("Canada").create(),
+                LookupItem.builder().code("USA").description("United States").create());
+    }
 
     @Override
     public ServiceFees getServiceFee(SubmissionFeeRequest submissionFeeRequest) {
