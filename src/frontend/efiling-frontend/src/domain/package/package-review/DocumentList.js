3caaa0e667b662ced0f2900e3a9385154df3c49c
Celina <yousiefc@pm.me>
2021-07-20 06:54:32 -0700

    updated tests

--- src/frontend/efiling-frontend/src/domain/package/package-review/DocumentList.js
+++ src/frontend/efiling-frontend/src/domain/package/package-review/DocumentList.js
@@ -102,7 +102,7 @@ export default function DocumentList({
               </span>
               <span className="label col-sm-4 d-lg-none">Action (s):</span>
               <span className="col-sm-8 col-lg-2 file-cell">
-                {!["WDRN", "REJ", "REF", "FIL"].some((i) =>
+                {document.status.code && !["WDRN", "REJ", "REF", "FIL"].some((i) =>
                   document.status.code.match(i)
                 ) && (
                   <>
