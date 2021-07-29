2d95131e102f1dafe709fd7c23af4b4cdcf0fb11
Brendan Beach <Brendan.Beach@gov.bc.ca>
2021-07-29 10:28:45 -0700

    Updated upload story

--- src/frontend/efiling-frontend/src/components/page/upload/Upload.stories.js
+++ src/frontend/efiling-frontend/src/components/page/upload/Upload.stories.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { getTestData } from "../../../modules/test-data/confirmationPopupTestData";
 import { getCourtData } from "../../../modules/test-data/courtTestData";
+import { getDocumentsData } from "../../../modules/test-data/documentTestData";
 
 import Upload from "./Upload";
 
@@ -12,11 +13,22 @@ export default {
 const confirmationPopup = getTestData();
 const courtData = getCourtData();
 const submissionId = "abc123";
-const upload = { confirmationPopup, submissionId, courtData };
+const files = getDocumentsData();
+const upload = { confirmationPopup, submissionId, courtData, files };
 
-export const Default = () => <Upload upload={upload} />;
+export const Default = () => {
+  const [showUpload, setShowUpload] = useState(true);
+  const [refreshFiles, setRefreshFiles] = useState(false);
+  const uploadWithStates = { ...upload, setShowUpload, setRefreshFiles };
+  return <Upload upload={uploadWithStates} />;
+};
 
-export const Mobile = () => <Upload upload={upload} />;
+export const Mobile = () => {
+  const [showUpload, setShowUpload] = useState(true);
+  const [refreshFiles, setRefreshFiles] = useState(false);
+  const uploadWithStates = { ...upload, setShowUpload, setRefreshFiles };
+  return <Upload upload={uploadWithStates} />;
+};
 
 Mobile.parameters = {
   viewport: {
