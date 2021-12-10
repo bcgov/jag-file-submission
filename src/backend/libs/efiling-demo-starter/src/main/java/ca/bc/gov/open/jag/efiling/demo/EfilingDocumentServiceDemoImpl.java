80cd2c61639b89520f43c179ad4ff25bc8322711
Celina <yousiefc@pm.me>
2021-12-10 14:53:51 -0800

    switched efiling demo mode rushRequired value back to true

--- src/backend/libs/efiling-demo-starter/src/main/java/ca/bc/gov/open/jag/efiling/demo/EfilingDocumentServiceDemoImpl.java
+++ src/backend/libs/efiling-demo-starter/src/main/java/ca/bc/gov/open/jag/efiling/demo/EfilingDocumentServiceDemoImpl.java
@@ -10,11 +10,11 @@ import java.util.List;
 public class EfilingDocumentServiceDemoImpl implements EfilingDocumentService {
     @Override
     public DocumentTypeDetails getDocumentTypeDetails(String courtLevel, String courtClass, String documentType) {
-        return new DocumentTypeDetails("This is a doc", documentType, BigDecimal.valueOf(7), true, false, false);
+        return new DocumentTypeDetails("This is a doc", documentType, BigDecimal.valueOf(7), true, true, false);
     }
 
     @Override
     public List<DocumentTypeDetails> getDocumentTypes(String courtLevel, String courtClass) {
-        return Arrays.asList(new DocumentTypeDetails("Description1", "AFF", BigDecimal.valueOf(7),true, false, false), new DocumentTypeDetails("Description2", "Type2", BigDecimal.valueOf(7),true, false, false), new DocumentTypeDetails("Description2", "POR", BigDecimal.valueOf(7),true, true, false));
+        return Arrays.asList(new DocumentTypeDetails("Description1", "AFF", BigDecimal.valueOf(7),true, true, false), new DocumentTypeDetails("Description2", "Type2", BigDecimal.valueOf(7),true, true, false), new DocumentTypeDetails("Description2", "POR", BigDecimal.valueOf(7),true, true, false));
     }
 }
