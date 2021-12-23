259c2e162d382e3ce642e56a56a05c03a96d35e7
Brendan Beach <55457785+BrendanBeachBC@users.noreply.github.com>
2021-12-23 11:20:01 -0800

    update to prod wsdls

--- src/backend/libs/efiling-cso-client/src/main/java/ca/bc/gov/open/jag/efilingcsoclient/CsoSubmissionServiceImpl.java
+++ src/backend/libs/efiling-cso-client/src/main/java/ca/bc/gov/open/jag/efilingcsoclient/CsoSubmissionServiceImpl.java
@@ -392,10 +392,10 @@ public class CsoSubmissionServiceImpl implements EfilingSubmissionService {
                 if(documentTypeDetail.getType().equals(documentTypeCd) && documentTypeDetail.isAutoProcessing()) {
                     csoFilingPackage.setAutomatedProcessYn(true);
                     //Set document processing
-                    document.setFilingProcessCd(AUTO_PROCESSING_STATE);
+//                    document.setFilingProcessCd(AUTO_PROCESSING_STATE);
                     csoFilingPackage.setDelayProcessing(determineDelayProcessing(document));
                 } else {
-                    document.setFilingProcessCd(MANUAL_PROCESSING_STATE);
+//                    document.setFilingProcessCd(MANUAL_PROCESSING_STATE);
                 }
             }
         }
