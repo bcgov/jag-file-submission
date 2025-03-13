498784e1da1339ec0f8b0e21e7ebfd27ae702fb5
TayGov <taylor.clausen@gov.bc.ca>
2025-03-13 09:57:15 -0700

    Update request for auto test

--- src/frontend/efiling-frontend/src/domain/submission/submission-history/SubmissionHistoryService.js
+++ src/frontend/efiling-frontend/src/domain/submission/submission-history/SubmissionHistoryService.js
@@ -1,10 +1,21 @@
 import axios from "axios";
 
 export const getSubmissionHistory = (applicationCode = "") => {
+  //Temporary fix for issue introduced
+  const token = localStorage.getItem("jwt");
+
   if (applicationCode) {
+
     return axios.get("/filingpackages", {
       params: { parentApplication: applicationCode },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     });
   }
-  return axios.get("/filingpackages");
+  return axios.get("/filingpackages", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 };
